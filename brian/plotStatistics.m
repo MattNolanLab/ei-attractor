f249cda28c30335da03ad247b041c8c21a907d8d
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2010-07-08 10:40:39 +0000

    plotStatistics.m is now a function - taking fileBase and the range of neurons to plot as arguments

--- brian/plotStatistics.m
+++ brian/plotStatistics.m
@@ -1,53 +1,61 @@
-% Print statistics about the data
+function [spikeFig rateFig] = plotStatistics(fileBase, neuronIDs)
 
-close all;
-clear all;
+    fileBase
+    neuronIDs
 
-% dir = 'results/fig/';
-% params = 's64_alpha0.01';
-% spikePlotFile = [dir 'spikeMap_' params '.eps'];
-% ratePlotFile  = [dir 'rateMap_'  params '.eps'];
-loadDir = 'results/';
-saveDir = 'results/fig/';
-fileBase = '2010-07-07T13-30-41';
+    errCode = 0;
 
-loadFile = [loadDir fileBase '_output.mat'];
-load(loadFile);
+    % Print statistics about the data
 
-[opt.sheet_size opt.alpha] = parseOptions(options);
-optStr = ['_s' num2str(opt.sheet_size) '_alpha' num2str(opt.alpha)];
-spikePlotFile = [saveDir fileBase optStr '_spikeMap.eps'];
-ratePlotFile =  [saveDir fileBase optStr '_rateMap.eps'];
+    close all;
+    %clear all;
+
+    % dir = 'results/fig/';
+    % params = 's64_alpha0.01';
+    % spikePlotFile = [dir 'spikeMap_' params '.eps'];
+    % ratePlotFile  = [dir 'rateMap_'  params '.eps'];
+    loadDir = 'results/';
+    saveDir = 'results/fig/';
+    %fileBase = '2010-07-07T13-30-41';
+
+    loadFile = [loadDir fileBase '_output.mat'];
+    load(loadFile);
+
+    [opt.sheet_size opt.alpha] = parseOptions(options);
+    optStr = ['_s' num2str(opt.sheet_size) '_alpha' num2str(opt.alpha)];
 
 
+    saveFig = true;
+    %saveFig = false;
 
-saveFig = true;
-%saveFig = false;
 
+    dt_rat = 0.02; % sec
+    endTime = 1200 - dt_rat; % sec
+    delta_t = 1; % sec
 
-dt_rat = 0.02; % sec
-endTime = 1200 - dt_rat; % sec
-delta_t = 1; % sec
+    h = 5.0;  % cm
+    arenaDiam = 180;   % cm
 
-h = 5.0;  % cm
-arenaDiam = 180;   % cm
+    for neuronNum = neuronIDs
+        disp(['Processing data for neuron ' int2str(neuronNum) '...']);
+        
+        spikePlotFile = [saveDir fileBase optStr '_n' int2str(neuronNum) '_spikeMap.eps'];
+        ratePlotFile =  [saveDir fileBase optStr '_n' int2str(neuronNum) '_rateMap.eps'];
 
-for i = 2049
-    neuronNum = i-1;
+        neuronSpikes = eval(['spikeMonitor_times_n' int2str(neuronNum)]);
+        %neuronSpikes = rat11015_t5c3_timeStamps;
 
-    neuronSpikes = eval(['spikeMonitor_times_n' int2str(neuronNum)]);
-    %neuronSpikes = rat11015_t5c3_timeStamps;
+        spikeFig = figure('Visible', 'off');
+        plotSpikes_xy(neuronSpikes, ratData_pos_x, ratData_pos_y, dt_rat, neuronNum);
+        if saveFig
+            print('-depsc', spikePlotFile);
+        end
 
-    figure(1);
-    plotSpikes_xy(neuronSpikes, ratData_pos_x, ratData_pos_y, dt_rat, neuronNum);
-
-    figure(2);
-    plotSNResponse(neuronSpikes, ratData_pos_x, ratData_pos_y, arenaDiam, h, dt_rat, neuronNum);
-    
-    if saveFig
-        figure(1);
-        print('-depsc', spikePlotFile);
-        figure(2);
-        print('-depsc', ratePlotFile);
+        rateFig = figure('Visible', 'off');
+        plotSNResponse(neuronSpikes, ratData_pos_x, ratData_pos_y, arenaDiam, h, dt_rat, neuronNum);
+        if saveFig
+            print('-depsc', ratePlotFile);
+        end
     end
+
 end
