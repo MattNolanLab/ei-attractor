be9ec3a610e7e93a22698143cd70d2b8a60b520c
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2010-07-31 19:00:00 +0000

    more sophisticated tracking version - always tracking only one blob. If the blob seems to go out of the sheet - new one is picked up. This way, the cumulation of error is reduced to minimum.

--- brian/trackPopulation.m
+++ brian/trackPopulation.m
@@ -24,10 +24,6 @@ function trackPopulation(fileName)
     
     firingPop = zeros(sheet_size, sheet_size);
     
-    % Set the initial blob position to the center of the response
-    blobPos_x = sheet_size/2;
-    blobPos_y = sheet_size/2;
-    
     % Preprocess spiking data: firing time --> histogram of firing, for
     % each neuron
     edges = linspace(0, endTime, endTime/dt_track);
@@ -46,8 +42,8 @@ function trackPopulation(fileName)
     end
     
     
-    blobPos_x = [0];
-    blobPos_y = [0];
+    blobPos_r = [0];
+    blobPos_c = [0];
     
     t_start = startTime+delta_t/2;
     t_end = endTime-delta_t/2;
@@ -55,45 +51,73 @@ function trackPopulation(fileName)
     firingPop = getFiringPop(spikeHist, t_start, dt_track, delta_t);
     [currBlobPos_r currBlobPos_c] = trackBlobs(firingPop);
     
+    % Find a blob nearest to the center of sheet
+    c_r = sheet_size/2;
+    c_c = sheet_size/2;
+    [cMin cMin_i] = min((currBlobPos_r - c_r).^2 + (currBlobPos_c - c_c).^2);
+    
+    currBlobPos_r = currBlobPos_r(cMin_i);
+    currBlobPos_c = currBlobPos_c(cMin_i);
+    
+    shift_r = currBlobPos_r - blobPos_r(end);
+    shift_c = currBlobPos_c - blobPos_c(end);
+    
     for t = t_start:dt_track:t_end
         firingPop = getFiringPop(spikeHist, t, dt_track, delta_t);
         [r, c] = trackBlobs(firingPop);
         
-        % Find the nearest blob to the last position
-        curr_r = repmat(currBlobPos_r', 1, size(r, 2));
-        curr_c = repmat(currBlobPos_c', 1, size(c, 2));
+        % Find the blob nearest to the last position
+        %curr_r = repmat(currBlobPos_r', 1, size(r, 2));
+        %curr_c = repmat(currBlobPos_c', 1, size(c, 2));
         
-        r = repmat(r, size(currBlobPos_r, 2), 1);
-        c = repmat(c, size(currBlobPos_c, 2), 1);
+        %r = repmat(r, size(currBlobPos_r, 2), 1);
+        %c = repmat(c, size(currBlobPos_c, 2), 1);
         
         % Don't change order of r - blobDist_r neither for c. It is
         % important for minimum in the next step
-        dist = (r-curr_r).^2 + (c-curr_c).^2;
+        
+        dist = (r-currBlobPos_r).^2 + (c-currBlobPos_c).^2;
         [minDist min_i] = min(dist);
-        [sortDist sort_i] = sort(minDist);
         
-        nBlobs = size(sort_i, 2);
-        quasiMedian_i = round(nBlobs/2);
+        %[sortDist sort_i] = sort(minDist);        
+        %nBlobs = size(sort_i, 2);
+        %quasiMedian_i = round(nBlobs/2);
         
-        new_r = r(1, sort_i(quasiMedian_i-2:quasiMedian_i+2));
-        new_c = c(1, sort_i(quasiMedian_i-2:quasiMedian_i+2));
+        new_r = r(min_i);
+        new_c = c(min_i);
         
-        old_r = currBlobPos_r(min_i(sort_i(quasiMedian_i-2:quasiMedian_i+2)));
-        old_c = currBlobPos_c(min_i(sort_i(quasiMedian_i-2:quasiMedian_i+2)));
+        %old_r = currBlobPos_r(min_i(sort_i(quasiMedian_i-2:quasiMedian_i+2)));
+        %old_c = currBlobPos_c(min_i(sort_i(quasiMedian_i-2:quasiMedian_i+2)));
 
-        r_shift = mean(new_r - old_r);
-        c_shift = mean(new_c - old_c);
+        %r_shift = mean(new_r - old_r);
+        %c_shift = mean(new_c - old_c);
         
-        blobPos_x = [blobPos_x (blobPos_x(end) + c_shift)];
-        blobPos_y = [blobPos_y (blobPos_y(end) + r_shift)];
         
-        currBlobPos_r = r(1, :);
-        currBlobPos_c = c(1, :);
-        t
+        blobPos_r = [blobPos_r (new_r - shift_r)];
+        blobPos_c = [blobPos_c (new_c - shift_c)];
+
+        % Check if new position of the blob is too near the edge, if so
+        % choose new blob near the center and reset shift
+        minEdgeDist =  10;
+        if (sheet_size - new_r < minEdgeDist || new_r < minEdgeDist || ...
+            sheet_size - new_c < minEdgeDist || new_c < minEdgeDist)
+            disp('change of blob');
+            [cMin cMin_i] = min((r - c_r).^2 + (c - c_c).^2);
+            currBlobPos_r = r(cMin_i);
+            currBlobPos_c = c(cMin_i);
+            
+            shift_r = currBlobPos_r - blobPos_r(end);
+            shift_c = currBlobPos_c - blobPos_c(end);
+        else
+            currBlobPos_r = new_r;
+            currBlobPos_c = new_c;
+        end
+        
+        %t
     end
     
     figure('Visible', 'off');
-    plot(blobPos_x, blobPos_y);
+    plot(blobPos_c, blobPos_r);
     %xlim([0 sheet_size]); ylim([0 sheet_size]);
     axis square;
     
