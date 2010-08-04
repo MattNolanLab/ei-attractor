f8721c8fc49a43324acf2e0f3e05e2faef02a6ed
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2010-08-04 20:42:24 +0000

    path integration tests with alpha=0.015

--- brian/eddieBatch_path_integration_sheet_size.sh
+++ brian/eddieBatch_path_integration_sheet_size.sh
@@ -6,14 +6,14 @@
 # into the cluster
 ########################################
 
-F_SHEET_SIZE="40 44 48 52 56 60 64 68 72 76 80 84 88 92 96"
+F_SHEET_SIZE="64 68 72 76 80 84 88 92 96"
 
 F_TIME="1200"
 
 CONN_MULT="20"
 
 #F_ALPHA="0.013 0.014 0.015 0.016 0.017 0.018 0.019 0.020 0.021 0.023 0.025 0.027 0.03 0.035 0.04 0.045 0.050 0.055 0.060 0.07 0.08 0.09 0.100"
-F_ALPHA="0.013"
+F_ALPHA="0.015 0.017 0.019"
 
 F_INPUT="0.3"
 
@@ -29,7 +29,7 @@ F_THRESHOLD="-20"
 
 F_L="2"
 
-job_id=22000
+job_id=22100
 for alpha in $F_ALPHA; do
     for conn_mult in $CONN_MULT; do
         for input in $F_INPUT; do
@@ -50,7 +50,7 @@ while [ $repeat -le $REPEAT ]; do
     echo "lambda_net=$lambda_net"
     echo "threshold=$threshold"
 
-    ./eddieBatch_fiete_path_integration.sh $sheet_size $F_TIME $alpha $conn_mult $job_id $input $taum $taui $lambda_net $threshold $l_param&
+    qsub ./eddieBatch_fiete_path_integration.sh $sheet_size $F_TIME $alpha $conn_mult $job_id $input $taum $taui $lambda_net $threshold $l_param
 
     echo
 
