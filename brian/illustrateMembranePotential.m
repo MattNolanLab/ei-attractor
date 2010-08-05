c6522b389db579afd7944989e43460fecc07c8a8
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2010-08-05 19:02:23 +0000

    - figure for illustrating how membrane potential & firing changes when the rat moves has been finished. - Other minor updates

--- brian/illustrateMembranePotential.m
+++ brian/illustrateMembranePotential.m
@@ -32,7 +32,7 @@ for x_i = 0:(sheet_size-1)
 end
     
 SNList_nID = 39;
-neuronID = SNList(SNList_nID);
+neuronID = double(SNList(SNList_nID));
 
 
 fontSize = 14;
@@ -42,18 +42,18 @@ x_limits = [8 16]; %sec
 f = figure('Position', [1, 1, 700, 700]);
 
 % Plot membrane potential at the top and firing rate at the bottom
-subplot(18, 1, 8:12, 'XMinorTick','on', 'TickDir', 'out', 'FontSize', fontSize);
+subplot(18, 1, 8:12, 'TickDir', 'out', 'FontSize', fontSize);
 hold all;
-plot(SNMonitor_times, SNMonitor_values(SNList_nID, :)*miliSec);
+plot(SNMonitor_times, SNMonitor_values(SNList_nID, :)*miliSec, 'LineWidth', 2);
 axis tight;
 xlim(x_limits);
 box on;
 ylabel('V_{membrane} (mV)');
 
 nFiringRate = getNeuronFiringRate(spikeHist(neuronID, :), 0, endTime, dt_track, delta_t);
-subplot(18, 1, 15:18, 'XMinorTick','on', 'TickDir', 'out', 'FontSize', fontSize);
+subplot(18, 1, 15:18, 'TickDir', 'out', 'FontSize', fontSize);
 hold all;
-plot(0:dt_track:endTime, nFiringRate);
+plot(0:dt_track:endTime, nFiringRate, 'LineWidth', 2);
 axis tight;
 xlim(x_limits);
 box on;
@@ -67,13 +67,19 @@ firingPop(:, :, 3) = getFiringPop(spikeHist, 15, dt_track, delta_t);
 
 for it = 1:3
     sPlot_i = (it-1)*4;
-    subplot(18, 11, sPlot_i + [1 2 3   12 13 14   23 24 25   34 35 36    45 46 47   56 57 58], 'XMinorTick','on', 'TickDir', 'out', 'FontSize', fontSize);
+    subplot(18, 11, sPlot_i + [1 2 3   12 13 14   23 24 25   34 35 36    45 46 47   56 57 58], 'TickDir', 'out', 'FontSize', fontSize);
     pcolor(1:sheet_size,1:sheet_size,firingPop(:, :, it));
     axis square tight;
     shading flat;
 
     set(gca, 'XTick', [1 sheet_size]);
-    set(gca, 'YTick', [1 sheet_size])
+    set(gca, 'YTick', [1 sheet_size]);
+    
+    recN_x = mod(neuronID, sheet_size) + 1;
+    recN_y = floor(neuronID/sheet_size) + 1;
+    hold all;
+    plot(recN_x+1/5*sheet_size, recN_y+1/5*sheet_size, '.', 'Color', [0 1 0], 'MarkerSize', 20);
+    plot([recN_x recN_x+1/5*sheet_size], [recN_y recN_y + 1/5*sheet_size], 'Color', [0 1 0], 'LineWidth', 2); 
 end
 
 set(gcf,'PaperPositionMode','auto');
