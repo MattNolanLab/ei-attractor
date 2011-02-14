f14cd6edaa29834ec902f784dc68b57ec005ac30
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2011-02-14 17:07:52 +0000

    experiments directory created in src. This will be used to provide a directory for each experiment conducted to create simulation data and reproducible outputs. Source code should be svn cp'd to the appropriate directory.
    - noise parameter added to mult_bump_spiking_net

--- experiments/mult_bump_spiking_net/exp_001/fiete_path_integration.py
+++ experiments/mult_bump_spiking_net/exp_001/fiete_path_integration.py
@@ -30,7 +30,7 @@ optParser.add_option("-w", "--write-data", action="store_true",
         dest="write_data", default=False)
 optParser.add_option("-i", "--input", type="float", default=0.3, dest="input",
         help="Feedforward constant input to all neurons")
-optParser.add_option("--noise-sigma", type="float", default=0, dest="noise_sigma", help="Input current Gaussian noise sigma (nA)")
+optParser.add_option("--noise-sigma", type="float", default=0, dest="noise_sigma", help="Input current Gaussian noise sigma (mV)")
 optParser.add_option("-p", "--print-only-conn", action="store_true",
         dest="print_only_conn", default=False)
 optParser.add_option("--alpha", type="float", default=0.10315, dest="alpha",
