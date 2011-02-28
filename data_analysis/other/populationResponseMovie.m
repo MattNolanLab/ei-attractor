1ba0c3eca2329613d68a833291790ca528cae1a8
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2011-02-28 13:47:28 +0000

    movie generator for population response

---
+++ data_analysis/other/populationResponseMovie.m
@@ -0,0 +1,31 @@
+% Create population response movie
+close all;
+
+opt = parseOptions(options);
+
+endTime = 200;
+
+dt_track = 0.1;
+delta_t = 0.25; % Should be this value.
+
+
+disp 'Creating spike histogram';
+spikeHist = createSpikeHistCell(1:numel(spikeCell), spikeCell, ...
+    dt_track, 0, endTime);
+
+figure(1);
+set(gcf, 'Visible', 'off');
+
+disp 'Printing frames';
+it = 1;
+for t = 0:dt_track:endTime
+    t
+    firingPop = getFiringPop(spikeHist, t, dt_track, delta_t);
+    image(firingPop, 'Parent', gca);
+    axis equal tight;
+    colorbar;
+    
+    M(it) = getframe(gcf);
+    
+    it = it+1;
+end
