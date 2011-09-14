56e53da78b3aae88242b80bf0a33dc757e096af9
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2011-09-14 11:28:29 +0000

    sparseness correlation analysis

---
+++ oscillations/006_narrow_protocol/fig_Vm_correlations_sparseness.m
@@ -0,0 +1,63 @@
+close all;
+clearvars -except results;
+
+path('../include', path);
+
+
+%load e_input_current_output_19-Jul-2011;
+outputDir = 'output';
+outputNum = '003';
+
+nParam  = size(results, 1);
+nTrials = size(results, 2);
+
+D = results(1, 1).opt.D;
+dt = results(1,1).opt.dt;
+
+N_spikes = 25;
+
+f_lim = [0 200];
+
+%par_it = 1;
+trial_it = 1;
+
+
+for par_it = 1:numel(results(1,1).opt.sparseness_vec)
+    res = results(par_it, trial_it);
+    opt = res.opt;
+
+    x_lim = [4 10];
+
+    ti_start = x_lim(1)/opt.dt + 1;
+    ti_end = x_lim(2)/opt.dt + 1;
+    win_len = 0.5 / dt; %s
+
+    plot_opt.fontSize = 16;
+    plot_opt.x_lim = x_lim;
+
+    
+    parfor it1 = 1:size(res.Vmon.e, 1)
+        for it2 = it1+1:size(res.Vmon.e, 1)
+            figure('Position', [0 0 1600 800], 'Visible', 'off');
+
+            Vm_correlations_EE(res, it1, it2, ti_start, ti_end, win_len, plot_opt);
+
+            set(gcf,'PaperPositionMode','auto');    
+            print('-depsc2', sprintf('%s/%s_narrow_ramp_2Vm_exc_sparseness_%3.3f_trial_%.3d_nid_%.3d_%.3d.eps', ...
+                    outputDir, outputNum, opt.e_sparseness, trial_it, res.opt.Emon_i(it1), res.opt.Emon_i(it2)));    
+        end
+    end
+
+
+    parfor it1 = 1:size(res.Vmon.i, 1)
+        for it2 = it1+1:size(res.Vmon.i, 1)
+            figure('Position', [0 0 1600 800], 'Visible', 'off');
+
+            Vm_correlations_II(res, it1, it2, ti_start, ti_end, win_len, plot_opt);
+
+            set(gcf,'PaperPositionMode','auto');    
+            print('-depsc2', sprintf('%s/%s_narrow_ramp_2Vm_inh_sparseness_%3.3f_trial_%.3d_nid_%.3d_%.3d.eps', ...
+                    outputDir, outputNum, opt.e_sparseness, trial_it, res.opt.Emon_i(it1), res.opt.Emon_i(it2)));    
+        end
+    end
+end
