d9df03d00017364e4d8804c44973e94bc6825f08
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2012-02-27 20:51:00 +0000

    AEC by Brette et al. python version, using brian libraries seems to work - need moar data now!

---
+++ model_fitting/aec_brian/IV_curve.py
@@ -0,0 +1,113 @@
+from brian.library.electrophysiology import *
+from matplotlib.pyplot import *
+
+from scipy.io import loadmat
+from numpy import diff
+
+# This is a script which uses Brette et al. Active Electrode Compensation module
+# implemented in brian (done offline)
+
+###############################################################################
+def pickVPreSpike(Vm, t_after, V_th=0):
+    '''
+    Pick only voltage traces that are more than t_after time units after the
+    last spike. By setting V_th, one can fine_tune spike threshold.
+    '''
+    spike_times = spike_peaks(Vm, V_th)
+    time_ids = np.arange(len(Vm))
+    t_mask = np.ndarray(len(Vm), dtype=bool)
+    t_mask[:] = True
+    for t_spike in spike_times:
+        t_mask = np.logical_and(t_mask, np.logical_or(time_ids <= t_spike,
+            time_ids > t_spike+t_after))
+
+    return time_ids[t_mask]
+###############################################################################
+
+
+inFile = "../data/C_neutralisation/2012_02_16/cell4 008 Copy Export.mat"
+#inFile = "../data/IV_kernel_est/cell2/Data 004 Copy Export.mat"
+
+mV = 1e3
+pA = 1e12
+ms = 1e3
+figSize = (12, 8)
+
+data = loadmat(inFile)
+times = data['c001_Time'][0]
+V = data['c002_Membrane_Voltage_2'][0]
+I = data['c003_Current_2'][0]
+dt = times[1] - times[0]    # Assuming constant time step
+
+
+# Full kernel estimation procedure
+tstart = 0
+I_T = 20
+ksize = 10e-3/dt
+Vk = V[tstart/dt:I_T/dt]
+Ik = I[tstart/dt:I_T/dt]
+K_full, V_mean = full_kernel(Vk, Ik, ksize, full_output=True)
+
+# Electrode kernel
+start_tail = int(3e-3/dt)
+Ke, Km = electrode_kernel(K_full, start_tail, full_output=True)
+
+figure(figsize=figSize)
+subplot(311)
+plot(times[0:ksize], K_full)
+ylabel('Kernel($\Omega$)')
+subplot(312)
+plot(times[0:start_tail], Ke)
+ylabel('Electrode kernel ($\Omega$)')
+subplot(313)
+plot(times[0:ksize], Km)
+ylabel('Membrane kernel ($\Omega$)')
+xlabel('Time (s)')
+
+
+# Compensation
+Vcorr = AEC_compensate(V, I, Ke)
+figure(figsize=figSize)
+subplot(311)
+plot(times, V*mV)
+ylabel('Uncomp. $V_m$ (mV)')
+subplot(312)
+plot(times, I*pA)
+ylabel('Injected current $I_{in}$ (pA)')
+subplot(313)
+plot(times*ms, Vcorr*mV)
+ylabel('Compensated $V_m$')
+xlabel('Time (s)')
+
+
+# IV curve
+IV_tstart = 20/dt
+C = 220e-12 # TODO: C estimation
+t_after = int(200e-3/dt)
+time_id = pickVPreSpike(Vcorr, t_after)
+time_id = time_id[0:len(time_id)-1]
+time_id = time_id[time_id > IV_tstart]
+
+dV_pre = np.diff(Vcorr)[time_id]
+Vm_pre = Vcorr[time_id]
+Iin_pre = I[time_id]
+Im_pre = C*dV_pre/dt - Iin_pre
+
+figure(figsize=figSize)
+plot(times, Vcorr*mV)
+hold(True)
+plot(times[time_id], Vm_pre*mV)
+xlabel('Time (s)')
+ylabel('$V_m$ (mV)')
+
+figure(figsize=figSize)
+plot(Vm_pre*mV, Im_pre*pA, '.')
+xlabel('$V_m$ (mV)')
+ylabel('$I_m$ (pA)')
+
+
+
+
+show()
+
+
--- model_fitting/generate_currents.m
+++ model_fitting/generate_currents.m
@@ -9,6 +9,9 @@ clear all;
 
 pA = 1e12;
 
+noise_range = 1500e-12;
+noise_T = 20;
+
 tau_fast = 3*1e-3;
 tau_slow = 10*1e-3;
 
@@ -20,10 +23,13 @@ currentFactor = [250 350 450 550 650 750] * 1e-12;
 
 dt = 0.05e-3;
 T = 40;
-silence_period = 3;
+
+% Estimated kernel size - in order to use fast algorithm for kernel
+% estimation, last ker_sz points of uniform noise input current must be 0
+ker_sz = 50e-3/dt;
+
 
 X = [];
-silence = zeros(1, numel(0:dt:silence_period-dt));
 
 for curr_f = 1:numel(currentFactor)
     it = 1;
@@ -36,12 +42,18 @@ for curr_f = 1:numel(currentFactor)
 
             X = X_fast + X_slow + DC_bias(bias);
             X = X * currentFactor(curr_f);
-            X = [silence X silence];
+            
+            noise_uni = -noise_range + 2*noise_range * ...
+                rand(1, noise_T/dt);
+            noise_uni(end-ker_sz+1:end) = 0;
+            
+            X = [noise_uni X];
             t = [0:numel(X)-1] * dt;
 
             
-            save(sprintf('OrnsteinUhlenbeck_%dpA_%d.mat', ...
-                fix(currentFactor(curr_f)*pA), it), 'X', 't');
+            save(sprintf('OrnsteinUhlenbeck_uni_%dpA_%dpA_%d.mat', ...
+                    fix(noise_range*pA), fix(currentFactor(curr_f)*pA), ...
+                    it), 'X', 't');
 
             it = it+1;
         end
