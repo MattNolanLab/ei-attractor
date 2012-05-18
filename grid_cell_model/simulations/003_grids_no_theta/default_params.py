aec9d58c5cd0c0234a62f56a8530dc5b85cd10b7
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2012-05-18 16:33:07 +0000

    003: velocity estimation simulations

--- grid_cell_model/simulations/003_grids_no_theta/default_params.py
+++ grid_cell_model/simulations/003_grids_no_theta/default_params.py
@@ -36,17 +36,14 @@ defaultParameters = {
         "delay"               :   1.0,          # ms
 
         "rat_dt"              :   20.0,         # ms
-        "Ivel"                :   50.0,         # pA
-        "Ivel_max"            :   50.0,         # pA
         "ratVelFName"         : '../../../../data/hafting_et_al_2005/rat_trajectory_lowpass.mat',
         #"ratVelFName"         : '../../../../data/hafting_et_al_2005/rat_data_original.mat',
+        "gridSep"             : 40,             # cm
 
         "pAMPA_mu"            :   y_dim/2.0,
         "pAMPA_sigma"         :   0.5/6,
         "pGABA_sigma"         :   0.5/6,
-        "prefDirC"            :   4.0,
         "arenaSize"           :   180.0,        # cm
-        "gridsPerArena"       :   2.5,
 
         "NMDA_amount"         :   2.0,          # %
 
@@ -104,6 +101,7 @@ defaultParameters = {
         "ntrials"             :   1,
         
         "output_dir"          :   _defaultOutputDir,
+        "stateMonDuration"    :  2e3,           # ms
 
         "stim_spread"         :  1.0}
 
