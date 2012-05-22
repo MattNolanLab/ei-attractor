74633c42f17051afd5a8a107db7b66ab6062a90b
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2012-05-22 15:07:33 +0000

    005: simulation and submition scripts for different variants of the network (and velocity response estimations)

---
+++ grid_cell_model/simulations/005_figures/submit_theta_no_gamma.py
@@ -0,0 +1,66 @@
+#
+#   submit_theta_no_gamma.py
+#
+#   Submit job(s) to the cluster/workstation: no gamma oscillations (i.e. no
+#   uniform inhibitory feedback
+#
+#       Copyright (C) 2012  Lukas Solanka <l.solanka@sms.ed.ac.uk>
+#       
+#       This program is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from default_params import defaultParameters
+from common         import *
+
+import logging as lg
+
+
+lg.basicConfig(level=lg.DEBUG)
+
+
+EDDIE = False  # if eddie, submit on a cluster using qsub
+
+
+parameters = defaultParameters
+
+parameters['time']              = 5e3  # ms
+parameters['ndumps']            = 1
+
+parameters['placeT']            = 10e3      # ms
+
+parameters['bumpCurrentSlope']  = 1.175     # pA/(cm/s), !! this will depend on prefDirC !!
+parameters['gridSep']           = 70        # cm, grid field inter-peak distance
+startJobNum = 0
+numRepeat = 1
+
+# Workstation parameters
+programName         = 'python2.6 -i simulation_no_theta.py'
+blocking            = True
+
+# Cluster parameters
+eddie_scriptName    = 'eddie_submit.sh simulation_no_theta.py'
+qsub_params         = "-P inf_ndtc -cwd -j y -l h_rt=13:00:00 -pe memory-2G 2"
+qsub_output_dir     = parameters['output_dir']
+
+ac = ArgumentCreator(parameters)
+
+#iterparams = {
+#        'bumpCurrentSlope'  : [1.15, 1.175, 1.2]}
+#ac.insertDict(iterparams, mult=False)
+
+if EDDIE:
+    submitter = QsubSubmitter(ac, eddie_scriptName, qsub_params, qsub_output_dir)
+else:
+    submitter = GenericSubmitter(ac, programName, blocking=blocking)
+submitter.submitAll(startJobNum, numRepeat, dry_run=False)
