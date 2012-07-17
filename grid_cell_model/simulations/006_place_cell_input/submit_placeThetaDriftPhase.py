e5ed70a33229540fef79f80193daa414e9dd57d5
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2012-07-17 16:17:33 +0000

    006: jobs1700, 1800, 1900

---
+++ grid_cell_model/simulations/006_place_cell_input/submit_placeThetaDriftPhase.py
@@ -0,0 +1,78 @@
+#
+#   submit_placeThetaDriftPhase.py
+#
+#   Submit job(s) to the cluster/workstation
+#
+#       Copyright (C) 2012  Lukas Solanka <l.solanka@sms.ed.ac.uk>
+#       
+#       This program is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import numpy as np
+
+from default_params import defaultParameters
+from common         import *
+
+import logging as lg
+
+
+lg.basicConfig(level=lg.DEBUG)
+
+
+EDDIE = True  # if eddie, submit on a cluster using qsub
+
+
+parameters = defaultParameters
+
+parameters['time']              = 30e3  # ms
+#parameters['time']              = 1e3  # ms
+parameters['ndumps']            = 1
+
+parameters['thetaPlaceFreq']    = 8 # Hz
+#parameters['thetaPlacePhase']   = np.pi/2 # rad
+
+parameters['Iplace']            = 30 # pA
+
+parameters['bumpCurrentSlope']  = 1.05      # pA/(cm/s), !! this will depend on prefDirC !!
+parameters['gridSep']           = 60        # cm, grid field inter-peak distance
+parameters['theta_noise_sigma'] = 0         # pA
+
+parameters['output_dir']        = 'output'
+
+
+startJobNum =1900
+numRepeat = 10
+
+# Workstation parameters
+programName         = 'nice python2.6 simulation_placeThetaDriftPhase.py'
+blocking            = False
+
+# Cluster parameters
+eddie_scriptName    = 'eddie_submit.sh simulation_placeThetaDriftPhase.py'
+qsub_params         = "-P inf_ndtc -cwd -j y -l h_rt=00:50:00 -pe memory-2G 2"
+qsub_output_dir     = parameters['output_dir']
+
+ac = ArgumentCreator(parameters)
+
+iterparams = {
+    "thetaPlacePhase" : np.arange(0, np.pi, np.pi/10)
+    #'Iplace'    :   range(0, 260, 10)
+    #'gridSep' : [50, 60]
+}
+ac.insertDict(iterparams, mult=False)
+
+if EDDIE:
+    submitter = QsubSubmitter(ac, eddie_scriptName, qsub_params, qsub_output_dir)
+else:
+    submitter = GenericSubmitter(ac, programName, blocking=blocking)
+submitter.submitAll(startJobNum, numRepeat, dry_run=False)
