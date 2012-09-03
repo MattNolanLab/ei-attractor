dd96b1ccf6779101117e30c09ce6cd887322268f
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2012-09-03 13:56:28 +0000

    reverse-merge -r791:780

--- grid_cell_model/simulations/005_figures/submit_AMPA_gaussian.py
+++ grid_cell_model/simulations/005_figures/submit_AMPA_gaussian.py
@@ -22,8 +22,7 @@
 from default_params import defaultParameters
 from common         import *
 
-import logging  as lg
-import numpy    as np
+import logging as lg
 
 
 lg.basicConfig(level=lg.DEBUG)
@@ -34,10 +33,10 @@ EDDIE = True  # if eddie, submit on a cluster using qsub
 
 parameters = defaultParameters
 
-#parameters['time']              = 1199.9e3  # ms
-parameters['time']              = 2e3  # ms
+parameters['time']              = 1199.9e3  # ms
+#parameters['time']              = 6e3  # ms
 parameters['stateMonDur']       = parameters['time']
-parameters['ndumps']            = 1
+parameters['ndumps']            = 10
 
 parameters['prefDirC_e']        = 4
 parameters['prefDirC_i']        = 0
@@ -45,35 +44,26 @@ parameters['prefDirC_i']        = 0
 parameters['Iext_e_theta']      = 650       # pA
 parameters['Iext_i_theta']      = 50        # pA
 
-parameters['pAMPA_sigma']       = 0.5/6.0
+parameters['pAMPA_mu']          = 1.2/0.6
 
 parameters['AMPA_gaussian']     = 1         # bool
-parameters["g_AMPA_total"]      = 2250      # nS
-#parameters["g_AMPA_total"]      = 0        # nS
-parameters["g_GABA_total"]      = 320       # nS
-parameters["g_uni_GABA_total"]  = 300       # nS
+parameters["g_AMPA_total"]      = 4500      # nS
+parameters["g_GABA_total"]      = 400       # nS
+parameters["g_uni_GABA_total"]  = 125        # nS
 
-#parameters['sigmaIextGaussian'] = 0.6/6.0
-#parameters['shuffleIextGaussian'] = 0
-
-parameters['condAddPercSynapses_e'] = 50
-parameters['condAdd_e']             = 20.0  # nS
-
-parameters['tau_AMPA']          = 2 # ms
+parameters['placeT']            = 10e3      # ms
+parameters['placeDur']          = 100       # ms
 
 parameters['bumpCurrentSlope']  = 0.933     # pA/(cm/s), !! this will depend on prefDirC !!
 parameters['gridSep']           = 60        # cm, grid field inter-peak distance
 parameters['theta_noise_sigma'] = 0         # pA
 
-parameters['theta_ph_jit_mean_e'] = 0       # rad
-parameters['theta_ph_jit_spread_e'] = 2*np.pi/10.0  # rad
-
 parameters['stateRec_dt']       = 0.25      # ms
 parameters['output_dir']        = 'output/'
 
 
-startJobNum = 0
-numRepeat = 1
+startJobNum = 4400
+numRepeat = 10
 
 # Workstation parameters
 programName         = 'python2.6 simulation_AMPA_gaussian.py'
@@ -81,19 +71,19 @@ blocking            = False
 
 # Cluster parameters
 eddie_scriptName    = 'eddie_submit.sh simulation_AMPA_gaussian.py'
-qsub_params         = "-P inf_ndtc -cwd -j y -l h_rt=00:03:00 -pe memory-2G 1"
+qsub_params         = "-P inf_ndtc -cwd -j y -l h_rt=15:00:00 -pe memory-2G 2"
 qsub_output_dir     = parameters['output_dir']
 
 ac = ArgumentCreator(parameters)
 
-iterparams = {
+#iterparams = {
+#    'Iplace'    :   [50, 100, 150, 200, 250]
+#    'gridSep' : [50, 60]
 #    'g_AMPA_total'  : [3600, 3800, 400, 4200]
-#    'pAMPA_sigma'       : np.arange(1.0, 2.0, 0.1) * defaultParameters['pAMPA_sigma'],
-#    'g_GABA_total'      : np.arange(0.5, 1.0, 0.1) * defaultParameters['g_GABA_total'],
-#    'g_uni_GABA_total'  : np.arange(0.5, 1.0, 0.1) * defaultParameters['g_uni_GABA_total']
-    'Iext_i_theta'      : [50, 100, 150, 200]
-}
-ac.insertDict(iterparams, mult=True, printout=True)
+#    'Iext_e_const'  : [375, 400, 425, 450]
+#    'g_uni_GABA_total'  : [160, 200, 240, 280]
+#}
+#ac.insertDict(iterparams, mult=False)
 
 if EDDIE:
     submitter = QsubSubmitter(ac, eddie_scriptName, qsub_params, qsub_output_dir)
