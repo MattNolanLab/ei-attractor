d275e3f03acc43518e901f68dbca4dbd19aaf488
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2012-10-25 14:51:24 +0000

    simulating E-surround and I-surround variants

--- grid_cell_model/simulations/005_figures/submit_fig_model.py
+++ grid_cell_model/simulations/005_figures/submit_fig_model.py
@@ -37,44 +37,50 @@ parameters = defaultParameters
 parameters['time']              = 6e3   # ms
 parameters['theta_start_mon_t'] = 1.0e3   # ms
 
-parameters['prefDirC_e']        = 4
+parameters['prefDirC_e']        = 0
 parameters['prefDirC_i']        = 0
 
+surround_type = "I_surround"
 
 ##############################################################################
 ## E-surround (AMPA_ring_like params)
 #
-#parameters['theta_noise_sigma'] = 0         # pA
+if surround_type == "E_surround":
+    parameters['theta_noise_sigma'] = 0         # pA
 ##############################################################################
 
 
 
 ##############################################################################
 # I_surround (AMPA_gaussian_params)
-parameters['Iext_e_theta']      = 650       # pA
-parameters['Iext_i_theta']      = 50        # pA
-
-parameters['pAMPA_mu']          = 1.2/0.6
-
-parameters['AMPA_gaussian']     = 1         # bool
-parameters["g_AMPA_total"]      = 4500      # nS
-parameters["g_GABA_total"]      = 400       # nS
-parameters["g_uni_GABA_total"]  = 125        # nS
-
-parameters['placeT']            = 10e3      # ms
-parameters['placeDur']          = 100       # ms
-
-parameters['bumpCurrentSlope']  = 0.933     # pA/(cm/s), !! this will depend on prefDirC !!
-parameters['gridSep']           = 60        # cm, grid field inter-peak distance
-parameters['theta_noise_sigma'] = 0         # pA
+elif surround_type == "I_surround":
+    parameters['Iext_e_theta']      = 650       # pA
+    parameters['Iext_i_theta']      = 50        # pA
+    
+    parameters['pAMPA_mu']          = 1.2/0.6
+    
+    parameters['AMPA_gaussian']     = 1         # bool
+    parameters["g_AMPA_total"]      = 4500      # nS
+    parameters["g_GABA_total"]      = 400       # nS
+    parameters["g_uni_GABA_total"]  = 125        # nS
+    
+    parameters['placeT']            = 10e3      # ms
+    parameters['placeDur']          = 100       # ms
+    
+    parameters['bumpCurrentSlope']  = 0.933     # pA/(cm/s), !! this will depend on prefDirC !!
+    parameters['gridSep']           = 60        # cm, grid field inter-peak distance
+    parameters['theta_noise_sigma'] = 0         # pA
 ##############################################################################
+else:
+    print "unknown surround profile type!"
+    exit(1)
 
 
-parameters['noise_sigma']       = 0.0       # mV
+#parameters['noise_sigma']       = 0.0       # mV
 
 
 
-startJobNum = 21
+startJobNum = 50
 numRepeat = 1
 
 # Workstation parameters
@@ -88,16 +94,17 @@ qsub_output_dir     = parameters['output_dir']
 
 ac = ArgumentCreator(parameters)
 
-#iterparams = {
-##        'Iext_e_theta' : [500, 525, 550, 575, 600, 625, 650, 700]
-##        'taum_e_spread' : [0.5, 0.75,   1, 1.25, 1.5, 1.75,   2, 2.25],
-##        'EL_e_spread'   : [0.5,    1, 1.5,    2, 2.5,    3, 3.5,    4]
-##         'theta_noise_sigma' : [0, 20, 40, 60, 80, 100, 120, 140]
-##        'g_GABA_total'  : [500, 525, 550, 575]
-##        'g_uni_GABA_total'  : [0, 25, 50, 75, 100, 125, 150, 175]
-##    'pAMPA_mu'  : np.array([0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2]) / 0.6
-#}
-#ac.insertDict(iterparams, mult=False)
+iterparams = {
+#        'Iext_e_theta' : [500, 525, 550, 575, 600, 625, 650, 700]
+#        'taum_e_spread' : [0.5, 0.75,   1, 1.25, 1.5, 1.75,   2, 2.25],
+#        'EL_e_spread'   : [0.5,    1, 1.5,    2, 2.5,    3, 3.5,    4]
+#         'theta_noise_sigma' : [0, 20, 40, 60, 80, 100, 120, 140]
+#        'g_GABA_total'  : [500, 525, 550, 575]
+#        'g_uni_GABA_total'  : [0, 25, 50, 75, 100, 125, 150, 175]
+#    'pAMPA_mu'  : np.array([0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2]) / 0.6
+        'noise_sigma'   : np.arange(2.1, 4.1, 0.1)
+}
+ac.insertDict(iterparams, mult=False)
 
 
 if EDDIE:
