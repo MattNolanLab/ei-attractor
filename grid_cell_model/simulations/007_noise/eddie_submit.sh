e1831b8f50f94eb7d5b3ad3690191fdee9b2f14d
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2012-11-16 16:04:36 +0000

    007_noise: noise (and heterogeneity) in the system and its influence on network dynamics

---
+++ grid_cell_model/simulations/007_noise/eddie_submit.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+#   eddie_submit.sh
+#
+#   Submit job to the cluster.
+#
+#       Copyright (C) 2012  Lukas Solanka <l.solanka@sms.ed.ac.uk>
+#       
+#       This program is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Initialise environment module
+
+. /etc/profile.d/modules.sh
+
+# Use python 2.6
+
+module load python/2.6.6
+
+
+BASE=../../
+export PYTHONPATH="/exports/work/inf_ndtc/s0966762/usr/local/lib/python2.6/site-packages:$BASE"
+
+export LOCAL_DIR=/exports/work/inf_ndtc/lsolanka
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LOCAL_DIR/usr/local/lib
+
+
+# Run the program
+python2.6 $* 
