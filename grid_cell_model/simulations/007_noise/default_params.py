0200740eedbe78b548dd4391975556e7d49f70cb
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2013-02-28 11:18:47 +0000

    Bump fitting simulation files
    007_noise/*_bump_fitting.py + eddie_submit --> cluster_submit

--- grid_cell_model/simulations/007_noise/default_params.py
+++ grid_cell_model/simulations/007_noise/default_params.py
@@ -28,9 +28,10 @@ _defaultOutputDir = "output/"
 y_dim       = np.sqrt(3)/2.
 
 defaultParameters = {
-        "Ne"                    :   68,
+        "Ne"                    :   34,
         "Ni"                    :   34,
         "delay"                 :   1.0,          # ms
+        "nthreads"              :   1,
 
         "rat_dt"                :   20.0,         # ms
         "ratVelFName"           : '../../../../data/hafting_et_al_2005/rat_trajectory_lowpass.mat',
@@ -46,16 +47,16 @@ defaultParameters = {
         "prefDirC_e"            :   4.0,
         "prefDirC_i"            :   0.0,
         "arenaSize"             :   180.0,        # cm
-        "Iplace"                :   250.0,        # pA
-        "placeDur"              :    100,         # ms
-        "placeSigma"            :      7,         # cm (CHECK)
+        #"Iplace"                :   250.0,        # pA
+        #"placeDur"              :    100,         # ms
+        #"placeSigma"            :      7,         # cm (CHECK)
 
         "NMDA_amount"           :   2.0,          # %
 
         "Iext_e_const"          :   300.0,        # pA
         "Iext_i_const"          :   200.0,        # pA
-        "Iext_start"            :   300.0,        # pA
-        "Iext_start_dur"        :   100.0,        # ms
+        #"Iext_start"            :   300.0,        # pA
+        #"Iext_start_dur"        :   100.0,        # ms
         "Iext_e_theta"          :   375.0,        # pA
         "Iext_i_theta"          :    25.0,        # pA
         "sigmaIextGaussian"     :     0.5,        # Normalised to <0, 1>
@@ -70,8 +71,8 @@ defaultParameters = {
 
         "taum_e"                :   9.3,          # ms
         "taum_e_spread"         :   0.31,         # ms
-        "EL_e"                  :   -68.5,        # mV
-        "EL_e_spread"           :   0.20,         # mV
+        "EL_e"                  : -68.5,        # mV
+        "EL_e_spread"           :   0.2,          # mV
         "Vt_e"                  :   -50,          # mV
         "Vr_e"                  :   -68.5,        # mV
         "gL_e"                  :   22.73,        # nS
@@ -83,9 +84,9 @@ defaultParameters = {
         "V_peak_e"              :   -40,          # mV
         
         "taum_i"                :   10,           # ms
-        "taum_i_spread"         :   0,            # ms
-        "EL_i"                  :   -60,          # mV
-        "EL_i_spread"           :   0,            # mV
+        "taum_i_spread"         :    0.0,         # ms
+        "EL_i"                  :  -60,           # mV
+        "EL_i_spread"           :    0.0,         # mV
         "Vt_i"                  :   -45,          # mV
         "Vr_i"                  :   -60,          # mV
         "gL_i"                  :   22.73,        # nS
@@ -99,7 +100,6 @@ defaultParameters = {
         "tau_AMPA"              :   1,            # ms
         "tau_NMDA_fall"         :    100,         # ms, only a single exponential used here
         "g_AMPA_total"          :   1400,         # nS
-        "g_uni_AMPA_total"      :    0,           # nS
         "uni_AMPA_density"      :   0.001,        # fraction
         "tau_GABA_A_rise"       :   0.1,          # ms
         "tau_GABA_A_fall"       :   5,            # ms
@@ -107,13 +107,15 @@ defaultParameters = {
         "g_uni_GABA_total"      :   28,           # nS
         "uni_GABA_density"      :   0.4,
 
-        "condAddPercSynapses_e" :   0.0,          # %
-        "condAdd_e"             :   0.0,          # nS
-    
         "E_AMPA"                :   0,            # mV
         "E_GABA_A"              :   -75,          # mV
+
+        "N_place_cells"         :   0,
+        "pc_max_rate"           :   100.0,         # Hz
+        "pc_field_std"          :   10.0,         # cm
+        "pc_conn_weight"        :   5.,          # nS
         
-        "noise_sigma"           :   2,            # mV
+        "noise_sigma"           :   100.0,        # pA
         "sigma_init_cond"       :   10,           # mV
         
         "sim_dt"                :   0.1,          # ms
