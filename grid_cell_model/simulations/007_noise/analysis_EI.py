733a158c8f625a07d0bbf2d3428f9a72a82e6ca8
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2013-04-01 15:32:40 +0100

    noise EI sensitivity analysis now plots the colormesh plot

--- grid_cell_model/simulations/007_noise/analysis_EI.py
+++ grid_cell_model/simulations/007_noise/analysis_EI.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 #
-#   analysis_peaks.py
+#   analysis_EI.py
 #
-#   Theta/gamma analysis using a custom "peak" method.
+#   Theta/gamma analysis using a custom "peak" method - E/I coupling parameter
+#   sweep.
 #
 #       Copyright (C) 2012  Lukas Solanka <l.solanka@sms.ed.ac.uk>
 #       
@@ -20,13 +21,68 @@
 #       along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 import numpy as np
+import matplotlib.pyplot as plt
 
-from parameters import JobTrialSpace2D
+from analysis.visitors    import AutoCorrelationVisitor
+from parameters           import JobTrialSpace2D
+from plotting.global_defs import globalAxesSettings, createColorbar
+import logging as lg
+lg.basicConfig(level=lg.DEBUG)
+
+
+# Other
+plt.rcParams['font.size'] = 12
+
+###############################################################################
+
+def plot2DTrial(sp, xlabel="", ylabel="", colorBar=True, title="",
+        clbarNTicks=2):
+    shape = sp.getShape()
+    rows = shape[0]
+    cols = shape[1]
+    trialNum = 0
+    acVal = np.ndarray(shape)
+    for r in xrange(rows):
+        for c in xrange(cols):
+            if (len(sp[r][c]) == 0):
+                acVal[r][c] = 0
+            else:
+                data = sp[r][c][trialNum].data['analysis']
+                acVal[r][c] = np.mean(data['acVal'])
+
+    ax = plt.gca()
+    globalAxesSettings(ax)
+    plt.pcolormesh(acVal)
+    createColorbar(ax, acVal, 'Correlation', nticks=clbarNTicks)
+    if (xlabel != ""):
+        plt.xlabel(xlabel)
+    if (ylabel != ""):
+        plt.ylabel(ylabel)
+
+    return acVal
 
 
 ###############################################################################
 
 #rootDir = 'output_local/2013-03-30T19-29-21_EI_param_sweep_0pA_small_sample'
-rootDir = 'output/2013-03-30T19-34-44_EI_param_sweep_0pA_full/'
-sp = JobTrialSpace2D((20, 20), rootDir)
+#shape = (2, 2)
 
+rootDir = 'output_local/2013-03-30T19-34-44_EI_param_sweep_0pA_full'
+shape = (20, 20)
+sp = JobTrialSpace2D(shape, rootDir)
+
+
+monName   = 'stateMonF_e'
+stateList = ['I_clamp_GABA_A']
+forceUpdate = False
+visitor = AutoCorrelationVisitor(monName, stateList, forceUpdate=forceUpdate)
+sp.visit(visitor)
+
+###############################################################################
+
+acVal = plot2DTrial(sp,
+        xlabel="E coupling strength (nS) TODO",
+        ylabel='I coupling strength (nS) TODO',
+        clbarNTicks=5)
+
+plt.show()
