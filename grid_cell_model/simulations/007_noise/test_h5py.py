c5c122a5744d992bf8cf21c9861904ef8eac9375
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2013-07-02 14:23:41 +0100

    no exceptions needed to reproduce the problem

--- grid_cell_model/simulations/007_noise/test_h5py.py
+++ grid_cell_model/simulations/007_noise/test_h5py.py
@@ -6,16 +6,12 @@ print(h5py.version.version)
 print(h5py.version.hdf5_version)
 print(h5py.version.api_version)
 
-
 nm = 'test_h5py.h5'
-try:
-    f = h5py.File(nm, 'r')
-    var = f['non-existent']
-    f.close()
-except:
-    print("Exception happened")
-    #f.close()
 
-
-f1 = h5py.File(nm, 'a')
+f1 = h5py.File(nm, 'w')
 f1.close()
+
+f2 = h5py.File(nm, 'r')
+
+f3 = h5py.File(nm, 'a')
+f3.close()
