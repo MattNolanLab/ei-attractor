703fadff363495fed30666bfcda7afffdda86abe
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2013-07-31 14:07:33 +0100

    add list support to HDF5 DataStorage (storage format change)
    A new version of DataStorage that now allows to work with lists stored in the
    HDF5 data file.
    Operations supported:
        - create a list
        - append to list (by using the append() method)
        - iteration over the list
    Operations NOT supported:
        - Deleting and inserting items at arbitrary locations
    
    TODO: some basic testing has been performed, but more extensive testing should
    be done on real data sets. All the lists in the old data format (in which the
    list indexes have been formatted with extra 0 suffixes will stop working). Any
    data like this will have to be converted.

--- grid_cell_model/test/pkg/data_storage/test_list_recursion.py
+++ grid_cell_model/test/pkg/data_storage/test_list_recursion.py
@@ -5,15 +5,11 @@ from data_storage import DataStorage
 
 d = DataStorage.open('list.h5', 'w')
 d1 = {"hola" : [10, 20, 30], "blabla": np.arange(20)}
-d['list'] = [1, 2, d1]
+d['recursive'] = d1
 d.close()
 
 print("\nReopening the file\n")
 d = DataStorage.open('list.h5', 'r+')
-l = d['list']
-print l
-l.append(10)
-print l
-d['list_append'] = l
-d['list'] = l
+d['recursive']['rec'] = d['recursive']
+d.close()
 
