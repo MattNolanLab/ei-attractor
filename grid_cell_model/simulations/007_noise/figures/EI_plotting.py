2e3dff94cb46fa7f7c9cb379d306eff3dad4967b
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2013-09-10 22:29:52 +0100

    add bump examples supplementary figure.
    - add cmap option to plot2DTrial
    - add EI_plotting.drawBumpExamples(): similar to drawGridExamples but plots
      bumps
    - add rectangle color option to EI_plotting.drawEIRectSelection()

--- grid_cell_model/simulations/007_noise/figures/EI_plotting.py
+++ grid_cell_model/simulations/007_noise/figures/EI_plotting.py
@@ -30,6 +30,7 @@ from matplotlib.gridspec import GridSpec
 from plotting.global_defs import globalAxesSettings, createColorbar
 from plotting.grids       import plotGridRateMap
 from parameters import DataSpace
+from figures_shared       import plotBump
 
 
 ###############################################################################
@@ -100,12 +101,12 @@ def plotACTrial(sp, varList, iterList, trialNumList=[0], xlabel="", ylabel="",
 
 def plotBumpSigmaTrial(sp, varList, iterList, thr=np.infty, trialNumList=[0],
         xlabel="", ylabel="", colorBar=True, clBarLabel="", vmin=None,
-        vmax=None, title="", clbarNTicks=2, xticks=True, yticks=True):
+        vmax=None, title="", clbarNTicks=2, xticks=True, yticks=True, cmap=None):
     C = aggregate2DTrial(sp, varList, trialNumList)
     C = ma.MaskedArray(C, mask=np.logical_or(np.isnan(C), C > thr))
     Y, X = computeYX(sp, iterList)
     return plot2DTrial(X, Y, C, xlabel, ylabel, colorBar, clBarLabel, vmin,
-            vmax, title, clbarNTicks, xticks, yticks)
+            vmax, title, clbarNTicks, xticks, yticks, cmap=cmap)
 
 def plotBumpErrTrial(sp, varList, iterList, thr=np.infty, mask=None,
         trialNumList=[0], xlabel="", ylabel="", colorBar=True, clBarLabel="",
@@ -143,14 +144,14 @@ def plotGridTrial(sp, varList, iterList, trialNumList=[0], r=0, c=0, xlabel="",
             vmax, title, clbarNTicks, xticks, yticks)
 
 
-def plot2DTrial(X, Y, C, xlabel="", ylabel="",
-        colorBar=True, clBarLabel="", vmin=None, vmax=None, title="",
-        clbarNTicks=2, xticks=True, yticks=True):
+def plot2DTrial(X, Y, C, xlabel="", ylabel="", colorBar=True, clBarLabel="",
+        vmin=None, vmax=None, title="", clbarNTicks=2, xticks=True,
+        yticks=True, cmap=None):
 
     ax = plt.gca()
     globalAxesSettings(ax)
     ax.minorticks_on()
-    plt.pcolor(X, Y, C, vmin=vmin, vmax=vmax)
+    plt.pcolor(X, Y, C, vmin=vmin, vmax=vmax, cmap=cmap)
     if (colorBar):
         if (clbarNTicks == None):
             createColorbar(ax, None, clBarLabel, orientation='horizontal',
@@ -250,6 +251,75 @@ def drawGridExamples(dataSpace, spaceRect, iterList, gsCoords, trialNum=0,
     return gs
 
 
+def drawBumpExamples(dataSpace, spaceRect, iterList, gsCoords, trialNum=0,
+        exIdx=(0, 0), xlabelPos=-0.2, xlabel2=True, ylabel2=True,
+        ylabelPos=-0.2, xlabel2Pos=-0.6, ylabel2Pos=-0.6, fontSize=None,
+        cmap=None):
+    '''
+    TODO: code duplication
+    '''
+    left   = spaceRect[0]
+    bottom = spaceRect[1]
+    right  = spaceRect[2]
+    top    = spaceRect[3]
+    exRow, exCol = exIdx
+
+    bumps = aggregate2D(dataSpace, ['analysis', 'bump_e', 'bump_e_rateMap'])
+
+    scaleBar = None
+    exRows = top - bottom + 1
+    exCols = right - left + 1
+    gs = GridSpec(exRows, exCols)
+    gsLeft   = gsCoords[0]
+    gsBottom = gsCoords[1]
+    gsRight  = gsCoords[2]
+    gsTop    = gsCoords[3]
+    gs.update(left=gsLeft, bottom=gsBottom, right=gsRight, top=gsTop)
+
+    we, wi = computeYX(dataSpace, iterList, r=exRow, c=exCol)
+    ax = None
+    for r in range(bottom, top+1):
+        for c in range(left, right+1):
+            print r, c
+            bump = bumps[r][c][trialNum]
+            if (not isinstance(bump, np.ndarray)):
+                continue
+
+            gsRow = top - r
+            gsCol = c - left
+            ax = plt.subplot(gs[gsRow, gsCol]) 
+            plotBump(ax, bump, cmap=cmap)
+
+            if (gsCol == 0):
+                label = "{0:.2f}".format(we[r][c])
+                ax.text(ylabelPos, 0.5, label, rotation=90,
+                        transform=ax.transAxes, va='center', ha='right',
+                        fontsize=fontSize)
+            if (gsRow == exRows - 1):
+                label = "{0:.2f}".format(wi[r][c])
+                ax.text(0.5, xlabelPos, label, transform=ax.transAxes,
+                        va='top', ha='center', fontsize=fontSize)
+            # Second Y label
+            if (ylabel2 and r-bottom == 0 and c-left == 0):
+                trans = transforms.blended_transform_factory(ax.transAxes,
+                        plt.gcf().transFigure)
+                weTxt_y = gsBottom + (gsTop - gsBottom)/2.0
+                ax.text(ylabel2Pos, weTxt_y, '$w_E$ (nS)', transform=trans,
+                        va='center', ha='right', rotation=90,
+                        fontsize=fontSize)
+            
+
+        # Second X label
+        if (xlabel2 and r - bottom == 0):
+            trans = transforms.blended_transform_factory(plt.gcf().transFigure,
+                    ax.transAxes)
+            wiTxt_x = gsLeft + (gsRight - gsLeft)/2.0
+            ax.text(wiTxt_x, xlabel2Pos, '$w_I$ (nS)', transform=trans, va='top',
+                    ha='center', fontsize=fontSize)
+
+    return gs
+
+
 def plotSquareGridExample(exLeft, exBottom, sz, fileName, exIdx, sweep_ax,
         sweepDataSpace, iterList, exGsCoords, figSize=(2.1, 2.1), fontSize=None,
         xlabel2=True, ylabel2=True):
@@ -271,7 +341,7 @@ def plotSquareGridExample(exLeft, exBottom, sz, fileName, exIdx, sweep_ax,
 
 
 
-def drawEIRectSelection(ax, spaceRect, X, Y):
+def drawEIRectSelection(ax, spaceRect, X, Y, color='black'):
     left   = spaceRect[0]
     bottom = spaceRect[1]
     right  = spaceRect[2]
@@ -281,6 +351,11 @@ def drawEIRectSelection(ax, spaceRect, X, Y):
     rectBottom = Y[bottom, left]
     rectRight  = X[top, right+1]
     rectTop    = Y[top+1, right]
+    #If left==0, we need to shrink the rectangle a little
+    dx = X[0, 1] - X[0, 0]
+    dy = Y[1, 0] - Y[0, 0]
+    if (left == 0):
+        rectLeft += 0.2*dx
     ax.add_patch(Rectangle((rectLeft, rectBottom), rectRight-rectLeft,
-        rectTop-rectBottom, facecolor='None', lw=1))
+        rectTop-rectBottom, facecolor='None', lw=1, edgecolor=color))
 
