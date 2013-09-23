896de8f718e92ee32343df59e941b91b1bacebd8
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2013-09-23 12:55:29 +0100

    figure4: parameterscape plots (all noise levels, 3 subfigures)
    There should be a lot of work on these plots yet: velocity integration and grid
    fields have to be added (simulations running right now).

--- grid_cell_model/plotting/parameterscape.py
+++ grid_cell_model/plotting/parameterscape.py
@@ -19,22 +19,25 @@
 #       along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 import numpy as np
+import matplotlib
+import matplotlib.cm as cm
 from matplotlib.cm import ScalarMappable
+from matplotlib.transforms import Bbox
+from matplotlib.ticker import MaxNLocator
 
 from global_defs import globalAxesSettings
 
-def parameterScape(ax, X, Y, C, fmts, sizes, cmaps, vranges, **kwargs):
+def parameterScape(fig, X, Y, C, fmts, sizes, cmaps, vranges, **kwargs):
     """
-      pcolor(C, **kwargs)
-      pcolor(X, Y, C, **kwargs)
+    """
+    ax = fig.gca()
 
-    """
-    #alpha = kwargs.pop('alpha', None)
-    #norm = kwargs.pop('norm', None)
-    #cmap = kwargs.pop('cmap', None)
-    #vmin = kwargs.pop('vmin', None)
-    #vmax = kwargs.pop('vmax', None)
-    #shading = kwargs.pop('shading', 'flat')
+    defaultMargin = 1.0
+    xmargin = kwargs.pop('xmargin', defaultMargin)
+    ymargin = kwargs.pop('ymargin', defaultMargin)
+    legend  = kwargs.pop('legend', None)
+    colorbars = kwargs.pop('colorbars', None)
+
     sm = []
     colors = []
     for cIdx, c in enumerate(C):
@@ -44,9 +47,6 @@ def parameterScape(ax, X, Y, C, fmts, sizes, cmaps, vranges, **kwargs):
         colors.append(m.to_rgba(c))
         sm.append(m)
 
-    print("X : {}".format(X))
-    print("Y : {}".format(Y))
-    print("colors : {}".format(colors))
 
     for yIdx, y in enumerate(Y):
         for xIdx, x in enumerate(X):
@@ -59,17 +59,76 @@ def parameterScape(ax, X, Y, C, fmts, sizes, cmaps, vranges, **kwargs):
     globalAxesSettings(ax)
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
-    #ax.spines['bottom'].set_visible(False)
-    #ax.spines['left'].set_visible(False)
-    ax.margins(0.05, 0.05)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.tick_params(bottom='off', top='off', left='off', right='off', length=0,
+            pad=-5)
+    ax.axis('scaled')
+    dx = X[1] - X[0]
+    dy = Y[1] - Y[0]
+    ax.set_xlim([X[0] - xmargin*dx, X[-1] + xmargin*dx])
+    ax.set_ylim([Y[0] - ymargin*dy, Y[-1] + ymargin*dy])
 
-    #minx = np.amin(x)
-    #maxx = np.amax(x)
-    #miny = np.amin(y)
-    #maxy = np.amax(y)
+    if (legend is not None):
+        legend_strings = legend[0]
+        legend_kw = legend[1]
+        if (legend_kw is None):
+            legend_kw = {}
+        parameterScapeLegend(ax, legend_strings, fmts, sizes, cmaps, kwargs,
+                **legend_kw)
 
-    #corners = (minx, miny), (maxx, maxy)
-    #self.update_datalim( corners)
-    #self.autoscale_view()
-    #self.add_collection(collection)
-    #return collection
+    if (colorbars is not None):
+        cax = plotColorbars(fig, sm, **colorbars)
+    else:
+        cax = None
+
+    return ax, cax
+
+
+def parameterScapeLegend(ax, leg, fmts, sizes, cmaps, plot_kwargs, **kwargs):
+    x = kwargs.pop('x', -0.1)
+    y = kwargs.pop('y', 1.1)
+    sizeScaleFactor = kwargs.pop('sizeScaleFactor', 1.5)
+    plot_kwargs['clip_on'] = False
+    colors = kwargs.pop('colors', np.linspace(0, 1, len(leg)))
+    print sizes
+
+    for idx, l in enumerate(leg):
+        color = cm.get_cmap(cmaps[idx])(colors[idx])
+        print color
+        ax.plot(x, y, fmts[idx], markersize=sizes[idx]*sizeScaleFactor,
+                color=color, transform=ax.transAxes, **plot_kwargs)
+
+
+def plotColorbars(fig, mappables, **kw):
+    labels = kw.pop('labels')
+    height = kw.pop('height', 0.01)
+    width  = kw.pop('width', 0.5)
+    x      = kw.pop('x', 0.5)
+    y      = kw.pop('y', 0.9)
+    div    = kw.pop('div', 0.05)
+    orientation = kw.get('orientation', 'horizontal')
+    kw['orientation'] = orientation
+    labelsize = kw.pop('labelsize', None)
+    tickLocators = kw.pop('tickLocators', None)
+
+    plotX = x
+    plotY = y
+    cbarAxes = []
+    for idx, m in enumerate(mappables):
+        cax = fig.add_axes(Bbox.from_bounds(plotX, plotY, width, height))
+        #globalAxesSettings(cax)
+        cbarAxes.append(cax)
+        cb = fig.colorbar(m, cax=cax, ticks=tickLocators[idx], **kw)
+        cb.set_label(labels[idx], size=labelsize)
+        if (orientation == 'horizontal'):
+            plotY += div
+        elif (orientation == 'vertical'):
+            plotX += div
+        else:
+            raise ValueError('Orientation must be either `horizontal` or `vertical`')
+
+
+    return cbarAxes
+
+
--- grid_cell_model/simulations/007_noise/figures/figure4.py
+++ grid_cell_model/simulations/007_noise/figures/figure4.py
@@ -22,6 +22,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot   import figure, plot, savefig
+from matplotlib.transforms import Bbox
+from matplotlib.ticker import MaxNLocator, MultipleLocator
 
 import numpy.ma as ma
 
@@ -58,7 +60,7 @@ velDataRoot = 'output_local/velocity'
 gridsDataRoot= 'output_local/grids_50pA_Ivel'
 shape = (31, 31)
 
-figSize = (8, 6)
+figSize = (7.5, 9)
 
 
 ###############################################################################
@@ -66,9 +68,12 @@ noise0   = 1
 noise150 = 1
 noise300 = 1
 
+transparent = False
+dpi = 300
+
 
 ###############################################################################
-def drawParamScape(ax, dataSpace, varList, trialNumList):
+def drawParamScape(fig, dataSpace, varList, trialNumList, noise_sigma):
     # Bump sigmas
     sigmaVarList = ['bump_e', 'sigma']
     CVarList = ['acVal']
@@ -82,13 +87,41 @@ def drawParamScape(ax, dataSpace, varList, trialNumList):
     Y, X = computeYX(dataSpace, iterList)
     Y = Y[:, 0]
     X = X[0, :]
-    parameterScape(ax, X, Y, [sigma, C, F],
+    legend = ['Bump sigma (neurons)', 'Correlatin', 'Frequency (Hz)']
+    legend_kw = dict(x=-0.02, y=1.07)
+    tickLocators = [MultipleLocator(10), MultipleLocator(0.2),
+            MultipleLocator(30)]
+    colorbars = dict(
+            labels=['Bump $\sigma$ (neurons)',
+                'Correlation',
+                'Oscillation frequency (Hz)'],
+            orientation='horizontal',
+            labelsize='small',
+            x = 0.32,
+            y = 0.85,
+            div = 0.06,
+            width = 0.3,
+            tickLocators=tickLocators)
+
+    ax, cax = parameterScape(fig, X, Y, [sigma, C, F],
             fmts=['o', 's', 's'],
-            sizes=[20, 10, 5],
+            sizes=[25, 12, 6],
             cmaps=['jet_r', 'jet', 'jet'],
-            vranges=[(0, 10), (-0.09, 0.675), (30, 90)])
+            vranges=[(0, 10), (-0.09, 0.675), (30, 90)],
+            legend=(legend, legend_kw),
+            xmargin=1.2,
+            ymargin=1.2,
+            colorbars=colorbars)
     ax.set_xlabel('$g_I$ (nS)')
     ax.set_ylabel('$g_E$ (nS)')
+
+    main_top = 0.8
+    nsX = 0.93
+    nsY = main_top + 0.02
+    ax.set_position(Bbox.from_extents([0.025, 0.07, 0.99, main_top]))
+    noise_sigma_txt = "$\sigma_{{noise}}$ = {0} pA".format(int(noise_sigma))
+    fig.text(nsX, nsY, noise_sigma_txt, va='center', ha='right', fontsize=19)
+
 
 
 
@@ -106,22 +139,22 @@ velDataSpace300 = JobTrialSpace2D(shape, velRoots[2])
 
 if (noise0):
     fig = plt.figure(figsize=figSize)
-    drawParamScape(plt.gca(), gammaDataSpace0, varList=None,
-            trialNumList=xrange(NTrials))
+    drawParamScape(fig, gammaDataSpace0, varList=None,
+            trialNumList=xrange(NTrials), noise_sigma=noise_sigmas[0])
     fname = outputDir + "/figure4_parameterscape_0pA.png"
-    savefig(fname, dpi=300, transparent=True)
+    savefig(fname, dpi=dpi, transparent=transparent)
 
 if (noise150):
     fig = plt.figure(figsize=figSize)
-    drawParamScape(plt.gca(), gammaDataSpace150, varList=None,
-            trialNumList=xrange(NTrials))
+    drawParamScape(fig, gammaDataSpace150, varList=None,
+            trialNumList=xrange(NTrials), noise_sigma=noise_sigmas[1])
     fname = outputDir + "/figure4_parameterscape_150pA.png"
-    savefig(fname, dpi=300, transparent=True)
+    savefig(fname, dpi=dpi, transparent=transparent)
 
 if (noise300):
     fig = plt.figure(figsize=figSize)
-    drawParamScape(plt.gca(), gammaDataSpace300, varList=None,
-            trialNumList=xrange(NTrials))
+    drawParamScape(fig, gammaDataSpace300, varList=None,
+            trialNumList=xrange(NTrials), noise_sigma=noise_sigmas[2])
     fname = outputDir + "/figure4_parameterscape_300pA.png"
-    savefig(fname, dpi=300, transparent=True)
+    savefig(fname, dpi=dpi, transparent=transparent)
 
