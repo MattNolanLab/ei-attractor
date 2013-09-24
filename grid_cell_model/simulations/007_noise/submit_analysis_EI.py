c995e81b368990e563c10a15ded46170ecfb1606
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2013-09-24 13:15:49 +0100

    add submitting through iterating over all noise_sigma (cherry picked from commit 483893ad1e05c14784dcecd80b6e669d7c9d808a)
    Conflicts:
    	grid_cell_model/simulations/007_noise/submit_analysis_EI.py
    
    Needed removing the no-theta stuff.

--- grid_cell_model/simulations/007_noise/submit_analysis_EI.py
+++ grid_cell_model/simulations/007_noise/submit_analysis_EI.py
@@ -26,7 +26,7 @@ import logging as lg
 lg.basicConfig(level=lg.DEBUG)
 
 # Submitting
-ENV         = 'workstation'
+ENV         = 'cluster'
 appName     = 'analysis_EI.py'
 rtLimit     = '00:02:00'
 numCPU      = 1
@@ -35,43 +35,42 @@ timePrefix  = False
 numRepeat   = 1
 dry_run     = False
 
-
-noise_sigma = 0 # pA
 gammaBumpType = 'gamma-bump'
 velocityType = 'velocity'
 gridsType = 'grids'
 
-
+noise_sigma_all = [0.0, 150.0, 300.0] # pA
 dirs = \
     ('output/even_spacing/gamma_bump', gammaBumpType, '{0}pA', (31, 31))
     #('output/grids_50pA_Ivel',  gridsType,     'EI_param_sweep_{0}pA', (30, 30))
     #('output/velocity',   velocityType,  'EI_param_sweep_{0}pA', (30, 30))
 
-p = {}
-simRootDir = dirs[0]
-p['type']  = dirs[1]
-simLabel   = dirs[2].format(int(noise_sigma))
-rowN       = dirs[3][0]
-colN       = dirs[3][1]
+for noise_sigma in noise_sigma_all:
+    p = {}
+    simRootDir = dirs[0]
+    p['type']  = dirs[1]
+    simLabel   = dirs[2].format(int(noise_sigma))
+    rowN       = dirs[3][0]
+    colN       = dirs[3][1]
 
-p['shapeRows'] = rowN
-p['shapeCols'] = colN
-p['forceUpdate'] = 1
+    p['shapeRows'] = rowN
+    p['shapeCols'] = colN
+    p['forceUpdate'] = 0
 
-###############################################################################
+    ###############################################################################
 
-ac = ArgumentCreator(p, printout=True)
+    ac = ArgumentCreator(p, printout=True)
 
-iterparams = {
-        'row' : np.arange(rowN),
-        'col' : np.arange(colN)
-}
-ac.insertDict(iterparams, mult=True)
+    iterparams = {
+            'row' : np.arange(rowN),
+            'col' : np.arange(colN)
+    }
+    ac.insertDict(iterparams, mult=True)
 
-###############################################################################
-submitter = SubmitterFactory.getSubmitter(ac, appName, envType=ENV,
-        rtLimit=rtLimit, output_dir=simRootDir, label=simLabel,
-        blocking=blocking, timePrefix=timePrefix, numCPU=numCPU)
-ac.setOption('output_dir', submitter.outputDir())
-startJobNum = 0
-submitter.submitAll(startJobNum, numRepeat, dry_run=dry_run)
+    ###############################################################################
+    submitter = SubmitterFactory.getSubmitter(ac, appName, envType=ENV,
+            rtLimit=rtLimit, output_dir=simRootDir, label=simLabel,
+            blocking=blocking, timePrefix=timePrefix, numCPU=numCPU)
+    ac.setOption('output_dir', submitter.outputDir())
+    startJobNum = 0
+    submitter.submitAll(startJobNum, numRepeat, dry_run=dry_run)
