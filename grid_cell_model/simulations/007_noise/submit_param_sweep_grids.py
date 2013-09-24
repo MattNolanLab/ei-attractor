c5f7869f924e175048a96dcdaa9a936aa7f790c0
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2013-09-24 23:34:56 +0100

    add grid field p. sweeps with new velocity (0-150pA)

--- grid_cell_model/simulations/007_noise/submit_param_sweep_grids.py
+++ grid_cell_model/simulations/007_noise/submit_param_sweep_grids.py
@@ -22,44 +22,48 @@
 import numpy as np
 from submitting.factory   import SubmitterFactory
 from submitting.arguments import ArgumentCreator
-from default_params       import defaultParameters as p
+from default_params       import defaultParameters as dp
 from param_sweep          import submitParamSweep, getBumpCurrentSlope
 import logging as lg
 #lg.basicConfig(level=lg.DEBUG)
 lg.basicConfig(level=lg.INFO)
 
-p['noise_sigma']       = 300.0     # pA
+noise_sigma_all = [0.0, 150.0, 300.0] # pA
 
-# Submitting
-ENV         = 'cluster'
-simRootDir  = 'output/grids_50pA_Ivel'
-simLabel    = 'EI_param_sweep_{0}pA'.format(int(p['noise_sigma']))
-appName     = 'simulation_grids.py'
-rtLimit     = '05:00:00'
-numCPU      = 1
-blocking    = True
-timePrefix  = False
-numRepeat   = 1
-dry_run     = False
+for noise_sigma in noise_sigma_all:
+    p = dp.copy()
+    p['noise_sigma'] = noise_sigma # pA
 
-p['time']              = 600e3  # ms
-p['nthreads']          = 1
-p['ntrials']           = 1
-p['velON']             = 1
+    # Submitting
+    ENV         = 'cluster'
+    simRootDir  = 'output/even_spacing/grids'
+    simLabel    = '{0}pA'.format(int(p['noise_sigma']))
+    appName     = 'simulation_grids.py'
+    rtLimit     = '05:00:00'
+    numCPU      = 1
+    blocking    = True
+    timePrefix  = False
+    numRepeat   = 1
+    dry_run     = False
+
+    p['time']              = 600e3  # ms
+    p['nthreads']          = 1
+    p['ntrials']           = 1
+    p['velON']             = 1
 
 
-# Range of parameters around default values
-Nvals        = 30    # Number of values for each dimension
-startFrac    = 0.
-endFrac      = 2.8572
+    # Range of E/I synaptic conductances
+    Nvals  = 31      # Number of values for each dimension
+    startG = 0.0     # nS
+    endG   = 6120.0  # nS
 
-extraIterparams = {'bumpCurrentSlope' : getBumpCurrentSlope(p['noise_sigma'],
-    threshold=0.05)}
-#extraIterparams['bumpCurrentSlope'] = [1.0]
+    extraIterparams = {'bumpCurrentSlope' : getBumpCurrentSlope(p['noise_sigma'],
+        threshold=0.05)}
+    #extraIterparams['bumpCurrentSlope'] = [1.0]
 
-###############################################################################
+    ###############################################################################
 
-submitParamSweep(p, startFrac, endFrac, Nvals, ENV, simRootDir, simLabel,
-        appName, rtLimit, numCPU, blocking, timePrefix, numRepeat, dry_run,
-        extraIterparams)
+    submitParamSweep(p, startG, endG, Nvals, ENV, simRootDir, simLabel,
+            appName, rtLimit, numCPU, blocking, timePrefix, numRepeat, dry_run,
+            extraIterparams)
 
