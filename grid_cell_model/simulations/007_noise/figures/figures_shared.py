25176de5798f6442b746cd4907403a4ebc39dae5
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2013-09-30 16:43:30 +0100

    add suppFigure_velocity: explanation of the bump speed tracking
    A slide-show of bump positions

--- grid_cell_model/simulations/007_noise/figures/figures_shared.py
+++ grid_cell_model/simulations/007_noise/figures/figures_shared.py
@@ -19,7 +19,6 @@
 #       along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 import numpy       as np
-import EI_plotting as EI
 import matplotlib.transforms as transforms
 
 from matplotlib.pyplot   import gca, axis, colorbar
