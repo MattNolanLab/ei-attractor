55c82f7ab262078f9ae8a836874201178dc29859
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2013-10-15 22:21:51 +0100

    signal manipulation routines (EI network) moved to data_storage.sim_models.ei
    move extractStateVariable, sumAllVariables, sliceSignal
    rename extractStateVars to extractSummedSignals
    
    All references should be updated (not tested with visitors).

--- sim_models/ei.py
+++ sim_models/ei.py
@@ -18,6 +18,75 @@ def extractSpikes(mon):
     return (e['senders'], e['times'])
 
 
+def sliceSignal(t, sig, tStart, tEnd):
+    idx = np.logical_and(t >= tStart, t <= tEnd)
+    return t[idx], sig[idx], idx
+
+
+def extractStateVariable(mon, nIdx, varStr):
+    '''Extract state variable from a monitor.
+    
+    Parameters
+    ----------
+    mon : list of dicts
+        A list of (NEST) monitors, each monitoring one neuron.
+    nIdx : int
+        Neuron index
+    varStr : str
+        Name of the variable
+    output
+        A tuple (data, dt), for the signal
+    '''
+    n = mon[nIdx]
+    return n['events'][varStr], n['interval']
+
+
+def sumAllVariables(mon, nIdx, varList):
+    '''
+    Extract all variables from the list of monitors and sum them. The variables
+    must implement the + operator.
+
+    Parameters
+    ----------
+    mon : a list of dicts
+        A list that contains dictionaries of monitors. The list should be
+        compatible with the extractStateVariable function.
+    nIdx : int
+        Neuron index
+    varList : list of strings
+        Contains the list of variables that whould be extracted from the
+        monitor and summed up.
+    output
+        A tuple (sum, dt) that contains the sum of all the variables 'sum' and
+        the sampling rate of the signals ('dt').
+    '''
+    sigSum = None
+    dtCheck = None
+    for idx in range(len(varList)):
+        sig, dt = extractStateVariable(mon, nIdx, varList[idx])
+        if (idx == 0):
+            sigSum = sig
+            dtCheck = dt
+        else:
+            assert(dtCheck == dt)
+            sigSum += sig
+
+    return sigSum, dt
+
+
+
+def extractSummedSignals(monList, varName, tStart, tEnd, monIdx=0):
+    '''
+    Extract state variables from a pair of monitors. One in the centre, the
+    other one at the edge of the neural sheet.
+    '''
+    t, dt = extractStateVariable(monList, monIdx, 'times')
+    sig, dt = sumAllVariables(monList, monIdx, varName)
+    t, sig, idx = sliceSignal(t, sig, tStart, tEnd)
+    return t, sig
+
+
+
 
 class MonitoredSpikes(spikes.PopulationSpikes):
     '''
@@ -42,4 +111,6 @@ class MonitoredSpikes(spikes.PopulationSpikes):
         N = data['net_attr'][NName]
         senders, times = extractSpikes(data[monName])
         spikes.PopulationSpikes.__init__(self, N, senders, times)
+
+
 
