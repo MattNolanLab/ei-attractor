f37f65ec1401f5ac99829a7fcb6f5470ecdb21fd
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2013-10-18 19:38:57 +0100

    submit velocity p. sweeps (detailed noise, EI-3_1)

--- grid_cell_model/simulations/007_noise/submit_param_sweep_velocity_noise.py
+++ grid_cell_model/simulations/007_noise/submit_param_sweep_velocity_noise.py
@@ -27,8 +27,8 @@ import logging as lg
 lg.basicConfig(level=lg.INFO)
 
 # We are expecting 1-3 distinct simulation runs
-simLabel = 'EI-1_3'
-#simLabel = 'EI-3_3'
+#simLabel = 'EI-1_3'
+simLabel = 'EI-3_1'
 
 p = dp.copy()
 # Submitting
@@ -55,9 +55,9 @@ noiseParams = SweepParams(0, 300, 31)
 if (simLabel == 'EI-1_3'):
     gEParams = SweepParams(816, 1224, 3)
     gIParams = SweepParams(2856, 3264, 3)
-elif (simLabel == 'EI-3_3'):
+elif (simLabel == 'EI-3_1'):
     gEParams = SweepParams(2856, 3264, 3)
-    gIParams = SweepParams(2856, 3264, 3)
+    gIParams = SweepParams(816, 1224, 3)
 else:
     raise ValueError('Unknown simLabel!')
 
