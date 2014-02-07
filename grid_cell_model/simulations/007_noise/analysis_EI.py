9c720c2cadeba366594385d17d59ff000b68d06b
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-02-07 10:23:09 +0000

    move grid_cell_model/analysis/visitors --> grid_cell_model/visitors/

--- grid_cell_model/simulations/007_noise/analysis_EI.py
+++ grid_cell_model/simulations/007_noise/analysis_EI.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python
 #
-#   analysis_EI.py
-#
-#   Theta/gamma analysis using a custom "peak" method - E/I coupling parameter
-#   sweep.
-#
-#       Copyright (C) 2012  Lukas Solanka <l.solanka@sms.ed.ac.uk>
-#       
-#       This program is free software: you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation, either version 3 of the License, or
-#       (at your option) any later version.
-#       
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
-#       
-#       You should have received a copy of the GNU General Public License
-#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+'''
+Perform analysis on whole 2D data sets.
+'''
 import matplotlib
 matplotlib.use('agg')
 
-import analysis.visitors as vis
+import visitors as vis
 from parameters import JobTrialSpace2D
 from submitting import flagparse
 
