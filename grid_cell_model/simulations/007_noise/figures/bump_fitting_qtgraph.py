4195325fcc8cc01f6dce8b2c3be8dfcc491d53ce
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-02-16 20:15:59 +0000

    mplot3d and pyqtgraph versions of bump fitting panels

--- grid_cell_model/simulations/007_noise/figures/bump_fitting_qtgraph.py
+++ grid_cell_model/simulations/007_noise/figures/bump_fitting_qtgraph.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python
-'''
-Create a 3D plot of the excitatory population firing rate snapshot, overlaid
-with a fitted Gaussian.
-'''
-
+from pyqtgraph.Qt import QtCore, QtGui
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
+import scipy.ndimage as ndi
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 from EI_plotting import aggregate as aggr
 from EI_plotting.base     import NoiseDataSpaces
@@ -48,13 +44,19 @@ class GaussianGenerator(object):
         return A * np.exp(- ((x-mu_x)**2 + (y-mu_y)**2) / (2. * self.sigma**2))
 
 
+## Create a GL View widget to display data
+app = QtGui.QApplication([])
+w = gl.GLViewWidget()
+w.show()
+w.setWindowTitle('pyqtgraph example: GLSurfacePlot')
+w.setCameraPosition(distance=50)
+w.qglClearColor(pg.mkColor('w'))
+
+
 ##############################################################################
 # Do it!
 rateMapTypes = ['bump', 'rateMap_e']
 trialIdx = 0
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
 
 # Plot the firing rate snapshot (E cells)
 for ns_idx in [1]:
@@ -69,18 +71,43 @@ for ns_idx in [1]:
     X = X.ravel()
     Y = Y.ravel()
     rateMap = rateMap_e.ravel()
-    ax.plot_wireframe(X, Y, rateMap)
+    pos = np.vstack((X, Y, rateMap)).T
+    p1 = gl.GLScatterPlotItem(pos=pos, color=(0.5, 0.5, 1, 1), size=5)
+    p1.setGLOptions('translucent')
+    w.addItem(p1)
 
 
-## Plot the fitted Gaussian
-#mu = [0, 0]
-#sigma = 2.5
-#A = 10
-#x, y = np.mgrid[-17:17:0.25, -15:15:0.25]
-#f = GaussianGenerator(A, mu, sigma)(x, y)
-#ax.plot_surface(x, y, f, rstride=1, cstride=1, linewidth=0, antialiased=False,
-#        cmap='coolwarm', shade=True, alpha=0.5)
 
-plt.show()
-#fname = outputDir + "/bump_fitting_3d.png"
-#fig.savefig(fname)
+
+
+### Add a grid to the view
+#g = gl.GLGridItem()
+#g.scale(2,2,1)
+#g.setDepthValue(10)  # draw grid after surfaces since they may be translucent
+#w.addItem(g)
+
+
+
+### Manually specified colors
+#z = ndi.gaussian_filter(np.random.normal(size=(50,50)), (1,1))
+#x = np.linspace(-12, 12, 50)
+#y = np.linspace(-12, 12, 50)
+#colors = np.ones((50,50,4), dtype=float)
+#colors[...,0] = np.clip(np.cos(((x.reshape(50,1) ** 2) + (y.reshape(1,50) ** 2)) ** 0.5), 0, 1)
+#colors[...,1] = colors[...,0]
+#
+#p3 = gl.GLSurfacePlotItem(z=z, colors=colors.reshape(50*50,4), shader='shaded', smooth=False)
+#p3.scale(16./49., 16./49., 1.0)
+#p3.translate(2, -18, 0)
+#w.addItem(p3)
+#
+
+
+## Start Qt event loop unless running in interactive mode.
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
+
+    w.grabFrameBuffer().save('bump3d.png')
+
