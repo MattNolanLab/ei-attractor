3c88140a1a8141b89913944ef7be93c4ff0258de
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-02-16 19:38:27 +0000

    refactor aggregate_bumps
      - user can select which data to aggregate (flagparse)
      - add const_position aggregation directory

--- grid_cell_model/simulations/007_noise/aggregate_bumps.py
+++ grid_cell_model/simulations/007_noise/aggregate_bumps.py
@@ -1,88 +1,81 @@
 #!/usr/bin/env python
-#
-#   aggregate_bumps.py
-#
-#   Aggregate bump data into the reductions file.
-#
-#       Copyright (C) 2012  Lukas Solanka <l.solanka@sms.ed.ac.uk>
-#       
-#       This program is free software: you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation, either version 3 of the License, or
-#       (at your option) any later version.
-#       
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
-#       
-#       You should have received a copy of the GNU General Public License
-#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+'''
+Aggregate bump data into the reductions file.
+'''
 import numpy as np
 from parameters  import JobTrialSpace2D
-import logging as lg
-#lg.basicConfig(level=lg.WARN)
-lg.basicConfig(level=lg.INFO)
+
+from submitting import flagparse
+
+parser = flagparse.FlagParser()
+parser.add_flag('--bump')
+parser.add_flag('--positions')
+parser.add_flag('--AC')
+parser.add_argument('--ntrials', type=int, default=5)
+parser.add_argument('--noLoadData', action='store_true')
+args = parser.parse_args()
 
 
 ns_all  = [0, 150, 300]
 ns_none = [-100]
 dirs = \
-    ("output/even_spacing/gamma_bump/{0}pA",     (31, 31), ns_all)
+    ("output/even_spacing/const_position/{0}pA",     (31, 31), ns_all)
+    #("output/even_spacing/gamma_bump/{0}pA",     (31, 31), ns_all)
     #("output/detailed_noise/gamma_bump/EI-3_1",  (31, 9),  ns_none)
     #("output/detailed_noise/gamma_bump/EI-1_3",  (31, 9),  ns_none)
 
-NTrials = 5
-trialNumList = xrange(NTrials)
+trialNumList = xrange(args.ntrials)
 varListBase = ['analysis']
-loadData = True
+loadData = not args.noLoadData
 
 ################################################################################
 shape        = dirs[1]
 noise_sigmas = dirs[2]
 for noise_sigma in noise_sigmas:
     rootDir = dirs[0].format(int(noise_sigma))
-
     sp = JobTrialSpace2D(shape, rootDir)
-    for suffix in ['', '_full']:
-        sp.aggregateData(varListBase + ['bump_e'+suffix, 'sigma'],
+
+    if args.bump or args.all:
+        for suffix in ['', '_full']:
+            sp.aggregateData(varListBase + ['bump_e'+suffix, 'sigma'],
+                    trialNumList, funReduce=None, loadData=loadData,saveData=True,
+                    output_dtype='array')
+            sp.aggregateData(varListBase + ['bump_e'+suffix, 'err2'], trialNumList,
+                    funReduce=None, loadData=loadData, saveData=True,
+                    output_dtype='array')
+            sp.aggregateData(varListBase + ['bump_e'+suffix, 'bump_e_rateMap'],
+                    trialNumList, funReduce=None, loadData=loadData, saveData=True,
+                    output_dtype='list')
+            sp.aggregateData(varListBase + ['bump_i'+suffix, 'bump_i_rateMap'],
+                    trialNumList, funReduce=None, loadData=loadData, saveData=True,
+                    output_dtype='list')
+
+    if args.positions or args.all:
+        bumpPosVars = varListBase + ['bump_e', 'positions']
+        sp.aggregateData(bumpPosVars + ['A'],
                 trialNumList, funReduce=None, loadData=loadData,saveData=True,
-                output_dtype='array')
-        sp.aggregateData(varListBase + ['bump_e'+suffix, 'err2'], trialNumList,
-                funReduce=None, loadData=loadData, saveData=True,
-                output_dtype='array')
-        sp.aggregateData(varListBase + ['bump_e'+suffix, 'bump_e_rateMap'],
-                trialNumList, funReduce=None, loadData=loadData, saveData=True,
                 output_dtype='list')
-        sp.aggregateData(varListBase + ['bump_i'+suffix, 'bump_i_rateMap'],
-                trialNumList, funReduce=None, loadData=loadData, saveData=True,
+        sp.aggregateData(bumpPosVars + ['errSum'],
+                trialNumList, funReduce=None, loadData=loadData,saveData=True,
+                output_dtype='list')
+        sp.aggregateData(bumpPosVars + ['mu_x'],
+                trialNumList, funReduce=None, loadData=loadData,saveData=True,
+                output_dtype='list')
+        sp.aggregateData(bumpPosVars + ['mu_y'],
+                trialNumList, funReduce=None, loadData=loadData,saveData=True,
+                output_dtype='list')
+        sp.aggregateData(bumpPosVars + ['sigma'],
+                trialNumList, funReduce=None, loadData=loadData,saveData=True,
+                output_dtype='list')
+        sp.aggregateData(bumpPosVars + ['t'],
+                trialNumList, funReduce=None, loadData=loadData,saveData=True,
                 output_dtype='list')
 
-    bumpPosVars = varListBase + ['bump_e', 'positions']
-    sp.aggregateData(bumpPosVars + ['A'],
-            trialNumList, funReduce=None, loadData=loadData,saveData=True,
-            output_dtype='list')
-    sp.aggregateData(bumpPosVars + ['errSum'],
-            trialNumList, funReduce=None, loadData=loadData,saveData=True,
-            output_dtype='list')
-    sp.aggregateData(bumpPosVars + ['mu_x'],
-            trialNumList, funReduce=None, loadData=loadData,saveData=True,
-            output_dtype='list')
-    sp.aggregateData(bumpPosVars + ['mu_y'],
-            trialNumList, funReduce=None, loadData=loadData,saveData=True,
-            output_dtype='list')
-    sp.aggregateData(bumpPosVars + ['sigma'],
-            trialNumList, funReduce=None, loadData=loadData,saveData=True,
-            output_dtype='list')
-    sp.aggregateData(bumpPosVars + ['t'],
-            trialNumList, funReduce=None, loadData=loadData,saveData=True,
-            output_dtype='list')
 
-
-    sp.aggregateData(varListBase + ['acVal'], trialNumList, funReduce=np.mean,
-            loadData=loadData, saveData=True, output_dtype='array')
-    sp.aggregateData(varListBase + ['acVec'], trialNumList, funReduce=None,
-            loadData=loadData, saveData=True, output_dtype='list')
-    sp.aggregateData(varListBase + ['freq'], trialNumList, funReduce=np.mean,
-            loadData=loadData, saveData=True, output_dtype='array')
+    if args.AC or args.all:
+        sp.aggregateData(varListBase + ['acVal'], trialNumList, funReduce=np.mean,
+                loadData=loadData, saveData=True, output_dtype='array')
+        sp.aggregateData(varListBase + ['acVec'], trialNumList, funReduce=None,
+                loadData=loadData, saveData=True, output_dtype='list')
+        sp.aggregateData(varListBase + ['freq'], trialNumList, funReduce=np.mean,
+                loadData=loadData, saveData=True, output_dtype='array')
