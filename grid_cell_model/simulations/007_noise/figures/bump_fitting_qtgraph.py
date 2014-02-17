d9da777176348feef77d453b0ace58fbc514931c
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-02-17 10:11:44 +0000

    add the fitted Gaussian plot to 3D bump example (pyqtgraph)

--- grid_cell_model/simulations/007_noise/figures/bump_fitting_qtgraph.py
+++ grid_cell_model/simulations/007_noise/figures/bump_fitting_qtgraph.py
@@ -1,3 +1,5 @@
+import logging
+
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
@@ -6,18 +8,19 @@ import numpy as np
 
 from EI_plotting import aggregate as aggr
 from EI_plotting.base     import NoiseDataSpaces
+import analysis.image as image
 
 from submitting import flagparse
 parser = flagparse.FlagParser()
 args = parser.parse_args()
 
+logger = logging.getLogger(__name__)
 outputDir = "panels/"
 
 NTrials=10
 iterList  = ['g_AMPA_total', 'g_GABA_total']
 
 noise_sigmas = [0, 150, 300]
-exampleIdx   = [(0, 0), (0, 0), (0, 0)] # (row, col)
 gridsDataRoot= None
 bumpDataRoot= 'output_local/even_spacing/gamma_bump'
 velDataRoot = None
@@ -29,7 +32,6 @@ roots = NoiseDataSpaces.Roots(bumpDataRoot, velDataRoot, gridsDataRoot)
 ps    = NoiseDataSpaces(roots, shape, noise_sigmas)
 
 
-exampleRC = ( (5, 15), (15, 5) )
 
 
 class GaussianGenerator(object):
@@ -56,27 +58,69 @@ w.qglClearColor(pg.mkColor('w'))
 ##############################################################################
 # Do it!
 rateMapTypes = ['bump', 'rateMap_e']
+gaussianVarList = ['analysis', 'bump_e', 'positions']
+exampleRC = ( (5, 15), (15, 5) )
 trialIdx = 0
+gaussT = 9.5e3 # ms
 
 # Plot the firing rate snapshot (E cells)
 for ns_idx in [1]:
+    r = exampleRC[1][0]
+    c = exampleRC[1][1]
+
+    # Plot rate maps
     rateMaps, _, _ = aggr.aggregateType(ps.bumpGamma[ns_idx], iterList,
             rateMapTypes, NTrials)
-    r = exampleRC[0][0]
-    c = exampleRC[0][1]
     rateMap_e = rateMaps[r][c][trialIdx]
     [nY, nX] = rateMap_e.shape
 
-    X, Y = np.meshgrid(np.arange(nX), np.arange(nY))
-    X = X.ravel()
-    Y = Y.ravel()
+    X2D, Y2D = np.meshgrid(np.arange(nX), np.arange(nY))
+    X = X2D.ravel()
+    Y = Y2D.ravel()
     rateMap = rateMap_e.ravel()
     pos = np.vstack((X, Y, rateMap)).T
-    p1 = gl.GLScatterPlotItem(pos=pos, color=(0.5, 0.5, 1, 1), size=5)
-    p1.setGLOptions('translucent')
-    w.addItem(p1)
+    pRates = gl.GLScatterPlotItem(pos=pos, color=(1., 0, 0, 1), size=5,
+            pxMode=True)
 
+    # Plot the fitted Gaussian
+    A = ps.bumpGamma[ns_idx].aggregateData(gaussianVarList + ['A'], [trialIdx],
+            output_dtype='list', loadData=True, saveData=False)
+    mu_x = ps.bumpGamma[ns_idx].aggregateData(gaussianVarList + ['mu_x'],
+            [trialIdx], output_dtype='list', loadData=True, saveData=False)
+    mu_y = ps.bumpGamma[ns_idx].aggregateData(gaussianVarList + ['mu_y'],
+            [trialIdx], output_dtype='list', loadData=True, saveData=False)
+    sigma = ps.bumpGamma[ns_idx].aggregateData(gaussianVarList + ['sigma'],
+            [trialIdx], output_dtype='list', loadData=True, saveData=False)
+    t = ps.bumpGamma[ns_idx].aggregateData(gaussianVarList + ['t'], [trialIdx],
+            output_dtype='list', loadData=True, saveData=False)
 
+    timeIdx = np.nonzero(t[r][c][trialIdx] <= gaussT)[0][-1]
+    A     = A[r][c][trialIdx][timeIdx]
+    mu_x  = mu_x[r][c][trialIdx][timeIdx]
+    mu_y  = mu_y[r][c][trialIdx][timeIdx]
+    sigma = sigma[r][c][trialIdx][timeIdx]
+    print("A: {0}, mu_x: {1}, mu_y: {2}, sigma: {3}".format(A, mu_x, mu_y,
+        sigma))
+    mu = image.Position2D(mu_x, mu_y)
+    xy = image.Position2D(X, Y)
+    torusSize = image.Position2D(nX, nY)
+    distances = image.remapTwistedTorus(mu, xy, torusSize)
+    gaussianZ = A * np.exp( - distances**2 / 2. / sigma**2 )
+    gaussPos = np.vstack((X, Y, gaussianZ)).T
+    Z2D = np.reshape(gaussianZ, (nY, nX)).T
+    pGaussian = gl.GLSurfacePlotItem(
+            x=np.arange(nX),
+            y=np.arange(nY),
+            z=Z2D,
+            color=(0.9, 0.9, 0.9, 0.1),
+            shader='shaded',
+            smooth=True)
+    #pGaussian = gl.GLScatterPlotItem(pos=gaussPos, color=(1, 0.5, 0.5, 1),
+    #        size=5)
+    w.addItem(pGaussian)
+    w.addItem(pRates)
+
+    
 
 
 
@@ -87,27 +131,11 @@ for ns_idx in [1]:
 #w.addItem(g)
 
 
-
-### Manually specified colors
-#z = ndi.gaussian_filter(np.random.normal(size=(50,50)), (1,1))
-#x = np.linspace(-12, 12, 50)
-#y = np.linspace(-12, 12, 50)
-#colors = np.ones((50,50,4), dtype=float)
-#colors[...,0] = np.clip(np.cos(((x.reshape(50,1) ** 2) + (y.reshape(1,50) ** 2)) ** 0.5), 0, 1)
-#colors[...,1] = colors[...,0]
-#
-#p3 = gl.GLSurfacePlotItem(z=z, colors=colors.reshape(50*50,4), shader='shaded', smooth=False)
-#p3.scale(16./49., 16./49., 1.0)
-#p3.translate(2, -18, 0)
-#w.addItem(p3)
-#
-
-
 ## Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
 
-    w.grabFrameBuffer().save('bump3d.png')
+    #w.grabFrameBuffer().save('bump3d.png')
 
