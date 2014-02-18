948a6276fba12ee943d2a19bd5aec61081d4ac5c
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-02-18 18:21:33 +0000

    synaptic strengths histograms have a break in the y axis
    This highlights the region with low histogram values (only I connections). E
    connections have normal y axis.

--- grid_cell_model/simulations/007_noise/figures/figure_connections.py
+++ grid_cell_model/simulations/007_noise/figures/figure_connections.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker     import MultipleLocator, AutoMinorLocator, \
         MaxNLocator
+import matplotlib.ticker as ti
 from matplotlib.transforms import Bbox
 
 import plotting.connections as pconn
@@ -40,6 +41,7 @@ args = parser.parse_args()
 
 def plotEToI(sp, gIdx, neuronIdx, trialNum=0, **kw):
     title = kw.pop('title', 'I cell')
+    ylim  = kw.pop('ylim', None)
 
     gE, gI = aggr.computeYX(sp, iterList)
     M      = sp[0][gIdx][trialNum].data['g_IE']
@@ -54,6 +56,7 @@ def plotEToI(sp, gIdx, neuronIdx, trialNum=0, **kw):
             transform=ax.transAxes)
     ax.set_xlim([0, annG])
     ax.set_xticks([0, annG])
+    ax.set_ylim(ylim)
 
 def plotIToE(sp, gIdx, neuronIdx, trialNum=0, **kw):
     title = kw.pop('title', 'E cell')
@@ -71,6 +74,58 @@ def plotIToE(sp, gIdx, neuronIdx, trialNum=0, **kw):
             transform=ax.transAxes)
     ax.set_xlim([0, annG])
     ax.set_xticks([0, annG])
+
+def plotIToEBrokenAxis(sp, gIdx, neuronIdx, trialNum=0, axBoundaries=[0, 0, 1, 1],
+        axesProportions=(0.5, 0.5), bottomLimits=None, topLimits=None,
+        **kw):
+    title = kw.pop('title', 'E cell')
+    fig   = kw.pop('fig', plt.gcf())
+    left, bottom, right, top = axBoundaries
+    h = top - bottom
+    w = right - left
+    hBottom = h*axesProportions[0]
+    hTop = h*axesProportions[1]
+    
+    axBottom = fig.add_axes(Bbox.from_extents(left, bottom, right, bottom +
+        hBottom))
+    axTop = fig.add_axes(Bbox.from_extents(left, top - hTop, right, top),
+            sharex=axBottom)
+
+    gE, gI = aggr.computeYX(sp, iterList)
+    M      = sp[0][gIdx][trialNum].data['g_EI']
+    conns  = M[neuronIdx, :]
+
+    pconn.plotConnHistogram(conns, title=title, ax=axBottom, **kw)
+    kw['ylabel'] = ''
+    pconn.plotConnHistogram(conns, title=title, ax=axTop, **kw)
+    annG = gI[0, gIdx]
+    if (annG - int(annG) == 0):
+        annG = int(annG)
+    ann = '$g_I$ = {0} nS'.format(annG)
+    fig.text(left+0.95*w, bottom+0.9*h, ann, ha='right', va='bottom',
+            fontsize='x-small')
+
+    axBottom.set_xlim([0, annG])
+    axBottom.set_xticks([0, annG])
+    axBottom.set_ylim(bottomLimits)
+    axBottom.set_yticks(bottomLimits)
+    axBottom.yaxis.set_minor_locator(ti.NullLocator())
+    axTop.set_ylim(topLimits)
+    axTop.set_yticks([topLimits[1]])
+    axTop.xaxis.set_visible(False)
+    axTop.spines['bottom'].set_visible(False)
+
+    divLen = 0.07
+    d = .015
+    kwargs = dict(transform=fig.transFigure, color='k', clip_on=False)
+    axBottom.plot((left-divLen*w, left+divLen*w), (bottom+hBottom + d,
+        bottom+hBottom - d), **kwargs)
+    axTop.plot((left-divLen*w, left+divLen*w), (top-hTop + d, top-hTop - d),
+            **kwargs)
+
+    return axBottom, axTop
+    
+
 
 
 def plotOutgoing(sp, gIdx, type, neuronIdx, trialNum=0, **kw):
@@ -149,7 +204,11 @@ if args.hists or args.all:
 
 
 if args.exampleHists or args.all:
-    exampleFigSize = (1.2, 1.2)
+    exampleFigSize = (1.6, 1.4)
+    exLeft   = 0.4
+    exBottom = 0.32
+    exRight  = 0.95
+    exTop    = 0.85
     exampleRC = ( (5, 15), (15, 5) )
     for exIdx, example in enumerate(exampleRC):
         kw = dict()
@@ -157,10 +216,14 @@ if args.exampleHists or args.all:
             kw['xlabel'] = ''
 
         fig = plt.figure(figsize=exampleFigSize)
-        ax = fig.add_axes(Bbox.from_extents(left, bottom, right, top))
-        plotEToI(sp, example[0], neuronIdx, ylabel='', title='', **kw)
-        ax.yaxis.set_visible(False)
-        ax.spines['left'].set_visible(False)
+        ax = fig.add_axes(Bbox.from_extents(exLeft, exBottom, exRight, exTop))
+        plotEToI(sp, example[0], neuronIdx, ylabel='', title='',
+                rwidth=0.8,
+                linewidth=0,
+                **kw)
+        #ax.yaxis.set_visible(False)
+        ax.yaxis.set_minor_locator(ti.NullLocator())
+        #ax.spines['left'].set_visible(False)
         ax.set_xlabel(ax.xaxis.get_label_text(), labelpad=-5)
         #fig.tight_layout(rect=[0.01, 0.01, 0.99, 0.99], pad=0)
         fname = outputDir + "/figure_connections_examples_E2I{0}.pdf"
@@ -169,12 +232,19 @@ if args.exampleHists or args.all:
 
 
         fig = plt.figure(figsize=exampleFigSize)
-        ax = fig.add_axes(Bbox.from_extents(left, bottom, right, top))
-        plotIToE(sp, example[1], neuronIdx, ylabel='', title='', **kw)
-        ax.yaxis.set_visible(False)
-        ax.spines['left'].set_visible(False)
-        ax.set_xlabel(ax.xaxis.get_label_text(), labelpad=-5)
-        #fig.tight_layout(rect=[0.01, 0.01, 0.99, 0.99], pad=0)
+        axBoundaries = (exLeft, exBottom, exRight, exTop)
+        axBottom, axTop = plotIToEBrokenAxis(sp, example[1], neuronIdx,
+                ylabel='', title='',
+                axBoundaries=axBoundaries,
+                axesProportions=(0.75, 0.2),
+                bottomLimits=(0, 60),
+                topLimits=(800, 900),
+                rwidth=0.8,
+                linewidth=0,
+                **kw)
+        axBottom.set_xlabel(axBottom.xaxis.get_label_text(), labelpad=-5)
+        fig.text(exLeft - 0.27, 0.5*(bottom+top), 'Count',
+                rotation=90, ha='center', va='center')
         fname = outputDir + "/figure_connections_examples_I2E{0}.pdf"
         plt.savefig(fname.format(exIdx), dpi=300, transparent=transparent)
         plt.close()
