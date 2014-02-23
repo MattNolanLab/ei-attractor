f8da1c9b205ad7257886a3d9886b345037a2de5b
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-02-23 13:51:18 +0000

    modify data pruning scripts to support cluster submission

--- grid_cell_model/simulations/007_noise/prune_data.py
+++ grid_cell_model/simulations/007_noise/prune_data.py
@@ -11,6 +11,7 @@ parser.add_argument("--where", type=str, required=True)
 parser.add_argument("--ns", type=int, choices=[0, 150, 300])
 parser.add_argument("--prune_what", type=str, required=True)
 parser.add_argument('--type', type=str, choices=['velocity'], required=True)
+parser.add_argument('--output_dir', type=str, help='Unused')
 parser.add_flag("--ns_all")
 o = parser.parse_args()
 
@@ -22,11 +23,7 @@ if (o.row is None) ^ (o.col is None):
 
 
 ns_all  = [0, 150, 300]
-ns_none = [-100]
-varListBase  = ['analysis']
-loadData     = True
-dirs = \
-    ('{0}/{1}pA', (31, 31), ns_all)
+shape   = (31, 31)
 
 if o.row is not None and o.col is not None:
     dataPoints = [(o.row, o.col)] 
@@ -34,10 +31,9 @@ else:
     dataPoints = None
 
 ################################################################################
-shape        = dirs[1]
 noise_sigmas = ns_all if o.ns_all  else [o.ns]
 for noise_sigma in noise_sigmas:
-    rootDir = dirs[0].format(o.where, int(noise_sigma))
+    rootDir = '{0}/{1}pA'.format(o.where, int(noise_sigma))
 
     sp = JobTrialSpace2D(shape, rootDir, dataPoints=dataPoints)
 
