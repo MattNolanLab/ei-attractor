5c0f7fc1af62abe62f0aec11b9d611feb2482b4c
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-03-13 14:15:35 +0000

    refactor figure_isBump (move stuff to default_settings)

--- grid_cell_model/simulations/007_noise/figures/default_settings.py
+++ grid_cell_model/simulations/007_noise/figures/default_settings.py
@@ -3,6 +3,9 @@ Default figure plotting and analysis settings.
 '''
 import matplotlib.pyplot as plt
 from matplotlib import rc
+from matplotlib.transforms import Bbox
+
+from EI_plotting.base     import NoiseDataSpaces
 
 plt.rcParams['font.size'] = 11
 
@@ -11,3 +14,35 @@ rc('pdf', fonttype=42)
 rc('mathtext', default='regular')
 
 iterList  = ['g_AMPA_total', 'g_GABA_total']
+figOutputDir = 'panels/'
+evenShape = (31, 31)
+noise_sigmas = [0, 150, 300]
+
+
+sweepFigSize = (3.7, 2.6)
+sweepLeft   = 0.08
+sweepBottom = 0.2
+sweepRight  = 0.8
+sweepTop    = 0.85
+sweepTransparent  = True
+
+
+def getDefaultSweepFig():
+    fig = plt.figure(figsize=sweepFigSize)
+    return fig, getDefaultSweepAxes(fig)
+
+def getDefaultSweepAxes(fig):
+    return fig.add_axes(Bbox.from_extents(sweepLeft, sweepBottom, sweepRight,
+        sweepTop))
+
+###############################################################################
+# Parameter spaces roots
+gridsDataRoot    = 'output_local/even_spacing/grids'
+bumpDataRoot     = 'output_local/even_spacing/gamma_bump'
+velDataRoot      = 'output_local/even_spacing/velocity_vertical'
+constPosDataRoot = 'output_local/even_spacing/const_position'
+
+def getDefaultParamSpaces():
+    roots = NoiseDataSpaces.Roots(bumpDataRoot, velDataRoot, gridsDataRoot,
+            constPos=constPosDataRoot)
+    return NoiseDataSpaces(roots, evenShape, noise_sigmas)
--- grid_cell_model/simulations/007_noise/figures/figure_isBump.py
+++ grid_cell_model/simulations/007_noise/figures/figure_isBump.py
@@ -1,42 +1,22 @@
 #!/usr/bin/env python
-#
 '''
 Figure showing where bumps are and where they are not.
 '''
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ti
-from matplotlib.gridspec   import GridSpec
-from matplotlib.colorbar   import make_axes
-from matplotlib.transforms import Bbox
 
-from EI_plotting          import sweeps, details, examples, rasters
+import default_settings as ds
+from EI_plotting          import sweeps, examples
 from EI_plotting          import aggregate as aggr
 from plotting.global_defs import globalAxesSettings, prepareLims
-from EI_plotting.base     import NoiseDataSpaces, getNoiseDataSpaces
-from parameters           import JobTrialSpace2D
-from analysis             import clustering
 from submitting import flagparse
 
-from matplotlib import rc
-rc('pdf', fonttype=42)
-rc('mathtext', default='regular')
 
-plt.rcParams['font.size'] = 11
+outputDir = ds.figOutputDir
+ps = ds.getDefaultParamSpaces()
 
-outputDir = "panels/"
-
-NTrials=10
-iterList  = ['g_AMPA_total', 'g_GABA_total']
-
-noise_sigmas = [0, 150, 300]
 exampleIdx   = [(0, 0), (0, 0), (0, 0)] # (row, col)
-gridsDataRoot    = 'output_local/even_spacing/grids'
-bumpDataRoot     = 'output_local/even_spacing/gamma_bump'
-velDataRoot      = 'output_local/even_spacing/velocity_vertical'
-constPosDataRoot = 'output_local/even_spacing/const_position'
-shape = (31, 31)
 
 parser = flagparse.FlagParser()
 parser.add_flag('--fracTotalSweep')
@@ -44,22 +24,10 @@ parser.add_flag('--isBump')
 parser.add_flag('--fracTotalHist')
 args = parser.parse_args()
 
-###############################################################################
-roots = NoiseDataSpaces.Roots(bumpDataRoot, velDataRoot, gridsDataRoot,
-        constPos=constPosDataRoot)
-ps    = NoiseDataSpaces(roots, shape, noise_sigmas)
-
-sweepFigSize = (3.7, 2.6)
-sweepLeft   = 0.08
-sweepBottom = 0.2
-sweepRight  = 0.8
-sweepTop    = 0.85
-transparent  = True
-
 
 ##############################################################################
 # Bump sigma sweeps
-fracTotalText = 'Fraction/simulation'
+fracTotalText = 'Bump quality'
 bumpTStart = 500.0
 bumpNTrials = 5
 bump_vmin = 0
@@ -74,16 +42,14 @@ bump_cbar_kw = dict(
 
 if args.fracTotalSweep or args.all:
     for ns_idx, noise_sigma in enumerate(ps.noise_sigmas):
-        fig = plt.figure(figsize=sweepFigSize)
-        ax = fig.add_axes(Bbox.from_extents(sweepLeft, sweepBottom, sweepRight,
-            sweepTop))
+        fig, ax = ds.getDefaultSweepFig()
         kw = dict(cbar=False, xlabel='', xticks=False)
         if ns_idx != 0:
             kw['ylabel'] = ''
             kw['yticks'] = False
         if ns_idx == 2:
             kw['cbar'] = True
-        data = aggr.IsBump(ps.bumpGamma[ns_idx], iterList, bumpNTrials,
+        data = aggr.IsBump(ps.bumpGamma[ns_idx], ds.iterList, bumpNTrials,
                 ignoreNaNs=True)
         _, _, cax = sweeps.plotSweep(data,
                 noise_sigma=noise_sigma,
@@ -102,15 +68,13 @@ if args.fracTotalSweep or args.all:
 bumpThreshold = 0.9
 if args.isBump or args.all:
     for ns_idx, noise_sigma in enumerate(ps.noise_sigmas):
-        fig = plt.figure(figsize=sweepFigSize)
-        ax = fig.add_axes(Bbox.from_extents(sweepLeft, sweepBottom, sweepRight,
-            sweepTop))
+        fig, ax = ds.getDefaultSweepFig()
         kw = dict(cbar=False, sigmaTitle=False)
         if ns_idx != 0:
             kw['ylabel'] = ''
             kw['yticks'] = False
         data = aggr.BumpFormationFilter(bumpThreshold,
-                ps.bumpGamma[ns_idx], iterList, bumpNTrials,
+                ps.bumpGamma[ns_idx], ds.iterList, bumpNTrials,
                 ignoreNaNs=True)
         _, _, cax = sweeps.plotSweep(data,
                 noise_sigma=noise_sigma,
@@ -123,19 +87,20 @@ if args.isBump or args.all:
         plt.close()
 
 
-histFigSize = (5, sweepFigSize[1])
+#histFigSize = (5, sweepFigSize[1])
 histBins = 40
 if args.fracTotalHist or args.all:
-    fig = plt.figure(figsize=sweepFigSize)
+    fig = plt.figure(figsize=ds.sweepFigSize)
     ax = fig.add_subplot(111)
     globalAxesSettings(ax)
-    data = aggr.IsBumpCollapsed(ps.bumpGamma, iterList, bumpNTrials,
+    data = aggr.IsBumpCollapsed(ps.bumpGamma, ds.iterList, bumpNTrials,
             ignoreNaNs=True)
     fractions, X, Y = data.getData()
     ax.hist(fractions, normed=True, rwidth=0.8, linewidth=0, bins=histBins)
-    ax.set_xlabel('Fraction of bump formed/simulation')
-    ax.set_ylabel('$p(\cdot)$')
-    ax.margins(0.01, 0.01)
+    ax.set_xlabel('Bump quality')
+    ax.set_ylabel('p(Bump quality)')
+    ax.margins(0.01, None)
+    ax.set_ylim(prepareLims((0, 20), 0.01))
     fig.tight_layout()
     fname = outputDir + "/bumps_isBumpFracTotal_hist.pdf"
     fig.savefig(fname, dpi=300, transparent=True)
