39ca02b9597b840814a2cf0faddfa0a7711c645a
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-04-08 18:07:43 +0100

    refactor FiringRateVisitor's thetaStartT
    replace it with tStart

--- grid_cell_model/visitors/spikes.py
+++ grid_cell_model/visitors/spikes.py
@@ -13,6 +13,10 @@ FRLogger = getClassLogger("FiringRateVisitor", __name__)
 __all__ = ['FiringRateVisitor', 'SpikeTrainXCVisitor', 'SpikeStatsVisitor']
 
 
+
+##############################################################################
+#                           Firing rates
+
 class FiringRateVisitor(DictDSVisitor):
     '''
     Determine various firing rate statistics of a population of neurons on the
@@ -21,7 +25,7 @@ class FiringRateVisitor(DictDSVisitor):
 
     '''
 
-    def __init__(self, winLen, winDt, thetaStartT=None, tEnd=None, forceUpdate=False):
+    def __init__(self, winLen, winDt, tStart=None, tEnd=None, forceUpdate=False):
         '''
         Initialize the visitor.
 
@@ -32,17 +36,14 @@ class FiringRateVisitor(DictDSVisitor):
             time (0, 1>.
         winDt : float (ms)
             ``dt`` of the firing rate window.
-        thetaStartT : float (ms)
-            Start time of the theta signal. The center of the firing rate
-            window will be in the middle of the theta signal. Therefore it is
-            up to the user to ensure that the peak of the theta signal is in
-            the middle. If None, extract from the data when performing analysis
+        tStart : float (ms)
+            Start time of the analysis.
         tEnd : float (ms)
             Analysis end time. If None, extract from the data
         forceUpdate : boolean, optional
             Whether to do the data analysis even if the data already exists.
         '''
-        self.thetaStartT = thetaStartT
+        self.tStart      = tStart
         self.tEnd        = tEnd
         self.winLen      = winLen
         self.winDt       = winDt
@@ -60,8 +61,7 @@ class FiringRateVisitor(DictDSVisitor):
             data['analysis'] = {}
         a = data['analysis']
 
-        thetaStartT = self._checkAttrIsNone(self.thetaStartT,
-                'theta_start_t', data)
+        tStart = self._checkAttrIsNone(self.tStart, 'theta_start_t', data)
         tEnd = self._checkAttrIsNone(self.tEnd, 'time', data)
 
         eSp = self._getSpikeTrain(data, 'spikeMon_e', ['Ne_x', 'Ne_y'])
@@ -69,7 +69,7 @@ class FiringRateVisitor(DictDSVisitor):
 
         if (not self.folderExists(a, ['FR_e']) or self.forceUpdate):
             FRLogger.info("Analysing (FR_e)")
-            eFR = eSp.avgFiringRate(thetaStartT, tEnd)
+            eFR = eSp.avgFiringRate(tStart, tEnd)
             a['FR_e'] = {
                     'all'             : eFR,
                     'avg'             : np.mean(eFR),
@@ -78,13 +78,12 @@ class FiringRateVisitor(DictDSVisitor):
             FRLogger.info("Data present (FR_e), skipping.")
 
 
-        import pdb; pdb.set_trace()
         frE = a['FR_e']
         if not ('popSliding' in frE.keys() and 'popSlidingTimes' in
                 frE.keys()):
             FRLogger.info("Analysing (sliding FR_e)")
             eSlidingFR, eSlidingFRt = eSp.slidingFiringRate(
-                                        thetaStartT, tEnd, self.winDt,
+                                        tStart, tEnd, self.winDt,
                                         self.winLen)
             frE.update({
                     'popSliding'      : np.mean(eSlidingFR, axis=0),
@@ -96,7 +95,7 @@ class FiringRateVisitor(DictDSVisitor):
 
         if (not self.folderExists(a, ['FR_i']) or self.forceUpdate):
             FRLogger.info("Analysing (FR_i)")
-            iFR = iSp.avgFiringRate(thetaStartT, tEnd)
+            iFR = iSp.avgFiringRate(tStart, tEnd)
             a['FR_i'] = {
                     'all'             : iFR,
                     'avg'             : np.mean(iFR)
@@ -110,7 +109,7 @@ class FiringRateVisitor(DictDSVisitor):
                 frI.keys()):
             FRLogger.info("Analysing (sliding FR_i)")
             iSlidingFR, iSlidingFRt = iSp.slidingFiringRate(
-                                        thetaStartT, tEnd, self.winDt,
+                                        tStart, tEnd, self.winDt,
                                         self.winLen)
             frI.update({
                     'popSliding'      : np.mean(iSlidingFR, axis=0),
@@ -119,9 +118,6 @@ class FiringRateVisitor(DictDSVisitor):
         else:
             FRLogger.info("Data present (sliding FR_i), skipping.")
 
-
-        
-        
 
 
 ##############################################################################
