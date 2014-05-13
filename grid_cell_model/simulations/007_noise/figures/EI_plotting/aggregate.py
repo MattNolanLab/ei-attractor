c216c21554633a6e94244014c3c40b1055a1baca
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-05-13 18:47:13 +0100

    fix for otherpkg

--- grid_cell_model/simulations/007_noise/figures/EI_plotting/aggregate.py
+++ grid_cell_model/simulations/007_noise/figures/EI_plotting/aggregate.py
@@ -8,7 +8,7 @@ import numpy as np
 import numpy.ma as ma
 
 from parameters import DataSpace
-from otherpkg.log import log_warn
+from grid_cell_model.otherpkg.log import log_warn
 from grid_cell_model.analysis.image import Position2D
 import grid_cell_model.analysis.image as image
 import grid_cell_model.analysis.signal as asignal
