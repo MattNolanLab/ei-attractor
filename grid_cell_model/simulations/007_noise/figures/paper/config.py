246e14c7a318dca7e33756b5204ad5c90d5b8e3b
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-07-25 14:28:33 +0100

    add raster example figures

--- grid_cell_model/simulations/007_noise/figures/paper/config.py
+++ grid_cell_model/simulations/007_noise/figures/paper/config.py
@@ -2,5 +2,24 @@
 '''Configuration file for the noise paper.'''
 from __future__ import absolute_import, print_function
 
+import matplotlib.ticker as ti
+
 def get_config():
-    return None
+    return _config
+
+
+_config = {
+    'RasterExamplePlotter' : {
+        'cbar_kw': dict(
+            label       = "max(E rate) (Hz)",
+            location    = 'right',
+            shrink      = 0.8,
+            pad         = -.02,
+            ticks       = ti.MultipleLocator(100),
+            rasterized  = True
+        ),
+        'ylabelPos': -0.1,
+        'markersize': 1.5,
+    }
+}
+
