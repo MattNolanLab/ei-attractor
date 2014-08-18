72d023d7aa8a5b4222e913350eb19018bd0405fd
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-08-18 11:33:15 +0200

    add supp. figure for plotting grid field examples

--- grid_cell_model/simulations/007_noise/figures/noisefigs/plotters/grids.py
+++ grid_cell_model/simulations/007_noise/figures/noisefigs/plotters/grids.py
@@ -1,9 +1,12 @@
 from __future__ import absolute_import, print_function
 
+import string
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ti
 from matplotlib.transforms import Bbox
+from matplotlib.backends.backend_pdf import PdfPages
 
 from grid_cell_model.parameters       import JobTrialSpace2D
 from grid_cell_model.data_storage     import DataStorage
@@ -19,6 +22,7 @@ from .base import FigurePlotter, SweepPlotter
 __all__ = [
     'GridSweepsPlotter',
     'GridExamplesPlotter',
+    'GridExampleRectPlotter',
     'VmExamplesPlotter',
     'GridDetailedNoisePlotter',
     'GridsDiffSweep',
@@ -98,7 +102,7 @@ class ContourGridSweepsPlotter(GridSweepsPlotter):
 
 
 ###############################################################################
-## Grid field examples
+## Grid field examples for the main figure
 class GridExamplesPlotter(FigurePlotter):
     exampleGridFName = "/grids_examples_{0}pA_{1}.pdf"
     exampleACFName = "/grids_examples_acorr_{0}pA_{1}.pdf"
@@ -143,9 +147,115 @@ class GridExamplesPlotter(FigurePlotter):
                 plt.close()
     
 
+###############################################################################
+## Grid field examples - plotted on the sheet
+class GridExampleRectPlotter(FigurePlotter):
+    def __init__(self, *args, **kwargs):
+        super(GridExampleRectPlotter, self).__init__(*args, **kwargs)
+
+    def drawSweep(self, ax, dataSpace, iterList, spaceRect, exIdx=(0, 0)):
+        cbar_kw= {'label' : 'Gridness score',
+            'orientation': 'vertical',
+            'shrink': 0.8,
+            'pad' : -0.05,
+            'ticks' : ti.MultipleLocator(0.5),
+            'rasterized' : True}
+
+        exRow, exCol = exIdx
+        Y, X = aggr.computeYX(dataSpace, iterList, r=exRow, c=exCol)
+        NTrials = 3
+
+
+        # Replot gridness score param. sweep
+        ax0 = plt.gca()
+        G = sweeps.plotGridTrial(dataSpace, ['gridnessScore'], iterList,
+                noise_sigma=None, sigmaTitle=False,
+                trialNumList=range(NTrials),
+                ax=ax0,
+                r=exRow,
+                c=exCol,
+                xlabel="$w_I$ (nS)",
+                ylabel="$w_E$ (nS)",
+                cbar=True, cbar_kw=cbar_kw,
+                vmin=None,
+                vmax=None)
+
+        examples.drawEIRectSelection(ax0, spaceRect, X, Y)
+
+    def drawA4RectExamples(self, dataSpace, noise_sigma, iterList, exRect,
+                           exIdx, letter=''):
+        fig = plt.figure(figsize=(8.27, 11.69))
+        margin    = 0.1
+        sw_left   = margin
+        sw_bottom = 0.82
+        sw_right  = 0.4
+        sw_top    = 0.96
+        div       = 0.075
+
+        letter_left = 0.03
+        letter_top_off = 0.01
+        letter_va='bottom'
+        letter_ha='left'
+
+        nsX = 0.9
+        nsY = sw_top
+
+        sweepsRect = sw_left, sw_bottom, sw_right-sw_left, sw_top-sw_bottom
+        ax_sweeps = fig.add_axes(sweepsRect)
+        self.drawSweep(ax_sweeps, dataSpace, iterList, exRect, exIdx)
+        fig.text(letter_left, sw_top+letter_top_off, letter, va=letter_va,
+                 ha=letter_ha, fontsize=19, fontweight='bold')
+
+        gsCoords = 0.12, 0.075, 0.92, sw_bottom-div
+        #gsCoords = margin, 0.46, 0.5, sw_bottom-div
+        gs = examples.drawGridExamples(dataSpace, exRect, iterList,
+                                       gsCoords=gsCoords, exIdx=exIdx, fig=fig)
+        #fig.text(letter_left, sw_bottom-div+letter_top_off, "B", va=letter_va,
+        #        ha=letter_ha, fontsize=19, fontweight='bold')
+        noise_sigma_txt = "$\sigma_{{noise}}$ = {0} pA".format(int(noise_sigma))
+        fig.text(nsX, nsY, noise_sigma_txt, va='center', ha='right', fontsize=19)
+
+    def plot(self, *args, **kwargs):
+        ps = self.env.ps
+        iter_list = self.config['iter_list']
+        YXRC = [(1, 22), (1, 22), (1, 22)] # (row, col)
+
+        exWidth = 8
+        exHeight = 10
+
+        exampleRC = [
+                [[1, 20], [20, 10], [3, 6]],
+                [[5, 2],  [15, 15], [1, 15]],
+                [[3, 7], [15, 15], [1, 20]]]
+        enable = [
+                [1, 1, 1],
+                [1, 1, 1],
+                [1, 1, 1]]
+
+        fname = self.config['output_dir'] + "/suppFigure_grid_examples.pdf"
+        outputPDF = PdfPages(fname)
+        strIdx = 0
+        for noise_idx, noise_sigma in enumerate(ps.noise_sigmas):
+            for exampleIdx, RC in enumerate(exampleRC[noise_idx]):
+                print(noise_idx, exampleIdx, RC)
+
+                exLeft   = RC[0]
+                exBottom = RC[1]
+                exRect = [exLeft, exBottom, exLeft+exWidth-1,
+                          exBottom + exHeight - 1]
+                self.drawA4RectExamples(ps.grids[noise_idx], noise_sigma,
+                                        iter_list, exRect, YXRC[noise_idx],
+                                        letter=string.ascii_uppercase[strIdx])
+                
+                outputPDF.savefig(dpi=300, transparent=False)
+                plt.close()
+                strIdx += 1
+
+        outputPDF.close()
+
+
 ##############################################################################
 # Membrane potential examples
-
 def openJob(rootDir, noise_sigma):
     fileTemplate = "noise_sigma{0}_output.h5"
     fileName = rootDir + '/' + fileTemplate.format(int(noise_sigma))
