8ad509ef7eb34d5d55ce3eb68dc8b8700075ae7f
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-11-30 16:32:18 +0000

    refactor aggregation scripts
    Especially cmd line parameter parsing had to be refactored in order to remove
    code duplication.

---
+++ grid_cell_model/simulations/007_noise/aggregate.py
@@ -0,0 +1,82 @@
+'''Aggregation helpers.'''
+from grid_cell_model.submitting import flagparse
+
+evenSpacingType = 'even-spacing'
+detailedNoiseType = 'detailed-noise'
+
+class AggregationParser(flagparse.FlagParser):
+    '''Parse arguments for aggregation scripts.'''
+    # Positions
+    allowedPositions = ['EI-1_3', 'EI-3_1']
+    detailedShape = (31, 9)
+    evenShape     = (31, 31)
+    allowedTypes = [evenSpacingType, detailedNoiseType]
+    ns_all = ['0pA', '150pA', '300pA']
+
+    def __init__(self, **kwargs):
+        super(AggregationParser, self).__init__(**kwargs)
+        self.add_argument("type", type=str, choices=self.allowedTypes,
+                          metavar='type',
+                          help=('Type of the aggregation. Can be one of %s' %
+                                str(self.allowedTypes)))
+        self.add_argument("where",      type=str, help='Root directory')
+        self.add_argument("--ns",       type=str, choices=['0pA', '150pA', '300pA'])
+        self.add_argument('--ntrials',  type=int)
+        self.add_argument('--position', type=str, choices=self.allowedPositions)
+        self.add_argument('--noLoadData', action='store_true')
+
+        self._opts = None
+
+    def parse_args(self, args=None, namespace=None):
+        '''Parse the arguments.'''
+        self._opts = super(AggregationParser, self).parse_args(args, namespace)
+        return self._opts
+
+    def _check_opts(self):
+        '''Check whether options have been parsed.'''
+        if self._opts is None:
+            raise RuntimeError("You need to parse the arguments first.")
+
+    @property
+    def noise_sigmas(self):
+        '''Get noise standard deviations.'''
+        self._check_opts()
+        if self.options.ns is None:
+            return self.ns_all
+        else:
+            return [self.options.ns]
+
+    @property
+    def detailed_positions(self):
+        '''Get detailed positions.'''
+        if self.options.position is None:
+            return self.allowedPositions
+        else:
+            return [self.options.position]
+
+    @property
+    def options(self):
+        '''Returned parsed options.'''
+        self._check_opts()
+        return self._opts
+
+    @property
+    def subdirs(self):
+        '''Return subdirectories to run through.'''
+        if self.options.type == evenSpacingType:
+            return self.noise_sigmas
+        else:
+            return self.detailed_positions
+
+    @property
+    def shape(self):
+        '''Return the 2D shapes.'''
+        if self.options.type == evenSpacingType:
+            return self.evenShape
+        else:
+            return self.detailedShape
+
+    @property
+    def load_data(self):
+        '''Whether to load data or not during aggregation.'''
+        return not self.options.noLoadData
--- grid_cell_model/simulations/007_noise/aggregate_velocity.py
+++ grid_cell_model/simulations/007_noise/aggregate_velocity.py
@@ -1,56 +1,33 @@
 #!/usr/bin/env python
-#
-#   aggregate_velocity.py
-#
-#   Aggregate velocity analysis outputs.
-#
-#       Copyright (C) 2012  Lukas Solanka <l.solanka@sms.ed.ac.uk>
-#       
-#       This program is free software: you can redistribute it and/or modify
-#       it under the terms of the GNU General Public License as published by
-#       the Free Software Foundation, either version 3 of the License, or
-#       (at your option) any later version.
-#       
-#       This program is distributed in the hope that it will be useful,
-#       but WITHOUT ANY WARRANTY; without even the implied warranty of
-#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#       GNU General Public License for more details.
-#       
-#       You should have received a copy of the GNU General Public License
-#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+'''Aggregate velocity analysis outputs.'''
 import numpy as np
-from parameters  import JobTrialSpace2D
-import logging as lg
-from submitting import flagparse
+from grid_cell_model.parameters import JobTrialSpace2D
 
-parser = flagparse.FlagParser()
-parser.add_argument('--noLoadData', action='store_true')
+from aggregate import AggregationParser
+
+parser = AggregationParser()
+parser.add_flag('--line_fits')
 args = parser.parse_args()
 
-ns_all  = [0, 150, 300]
-ns_none = [-100]
 varListBase  = ['analysis']
-loadData     = not args.noLoadData
-dirs = \
-    ('output/detailed_noise_vertical/velocity/EI-3_1', (31, 9), ns_all)
-    #('output/detailed_noise_vertical/velocity/EI-1_3', (31, 9), ns_all)
-    #('output/even_spacing/velocity_vertical/{0}pA', (31, 31), ns_all)
-    #('output/even_spacing/velocity/{0}pA', (31, 31), ns_all)
 
 ################################################################################
-shape        = dirs[1]
-noise_sigmas = dirs[2]
-for noise_sigma in noise_sigmas:
-    rootDir = dirs[0].format(int(noise_sigma))
+for subDir in parser.subdirs:
+    rootDir = '{0}/{1}'.format(args.where, subDir)
+    print rootDir, parser.shape
 
-    sp = JobTrialSpace2D(shape, rootDir)
-    sp.aggregateData(varListBase + ['lineFitErr'], funReduce=np.sum,
-            trialNumList='all-at-once', saveData=True, output_dtype='array',
-            loadData=loadData)
-    sp.aggregateData(varListBase + ['lineFitSlope'], funReduce=None,
-            trialNumList='all-at-once', saveData=True, output_dtype='array',
-            loadData=loadData)
-    sp.aggregateData(varListBase + ['bumpVelAll'], funReduce=None,
-            trialNumList='all-at-once', saveData=True, output_dtype='list',
-            loadData=loadData)
+    sp = JobTrialSpace2D(parser.shape, rootDir)
+
+    if args.line_fits or args.all:
+        sp.aggregateData(varListBase + ['lineFitErr'],
+                         funReduce=np.sum, trialNumList='all-at-once',
+                         saveData=True, output_dtype='array',
+                         loadData=parser.load_data)
+        sp.aggregateData(varListBase + ['lineFitSlope'],
+                         funReduce=None, trialNumList='all-at-once',
+                         saveData=True, output_dtype='array',
+                         loadData=parser.load_data)
+        sp.aggregateData(varListBase + ['bumpVelAll'],
+                         funReduce=None, trialNumList='all-at-once',
+                         saveData=True, output_dtype='list',
+                         loadData=parser.load_data)
