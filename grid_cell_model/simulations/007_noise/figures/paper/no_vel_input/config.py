f443f2ebfaccd7b626bd3b801fedebc28830c1f0
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2014-12-13 20:53:39 +0000

    add figures for grids without velocity inputs

---
+++ grid_cell_model/simulations/007_noise/figures/paper/no_vel_input/config.py
@@ -0,0 +1,26 @@
+
+'''Configuration file for the noise paper: no place cell input.'''
+from __future__ import absolute_import, print_function
+
+from matplotlib import ticker as ti
+
+def get_config():
+    return _config
+
+
+_config = {
+    'grids_data_root':      'simulation_data/submission/grids_no_velocity',
+    'bump_data_root':       None,
+    'vel_data_root':        None,
+    'const_pos_data_root':  None,
+    'singleDataRoot':       None,
+
+    'GridSweepsPlotter': {
+        'vmin': -.67,
+        'vmax': .42,
+
+        'cbar_kw': {
+            'ticks': ti.MultipleLocator(0.25),
+        },
+    },
+}
---
+++ grid_cell_model/simulations/007_noise/figures/paper/no_vel_input/figure_grids.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from __future__ import absolute_import, print_function
+
+from grid_cell_model.submitting import flagparse
+import noisefigs
+from noisefigs.env import NoiseEnvironment
+
+import config
+
+parser = flagparse.FlagParser()
+parser.add_flag('--grids')
+parser.add_flag('--examplesFlag')
+parser.add_flag('--diff_sweep')
+args = parser.parse_args()
+
+env = NoiseEnvironment(user_config=config.get_config())
+
+if args.grids or args.all:
+    env.register_plotter(noisefigs.plotters.GridSweepsPlotter)
+
+if args.examplesFlag or args.all:
+    env.register_plotter(noisefigs.plotters.GridExamplesPlotter)
+
+if args.diff_sweep or args.all:
+    env.register_plotter(noisefigs.plotters.GridsDiffSweep)
+
+env.plot()
