1e6c9ae71a641733d959405038669cd7c03c6f59
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2015-02-25 12:13:25 +0000

    test simulation code now saves all trials into a single file

--- grid_cell_model/simulations/007_noise/submit_test_EI.py
+++ grid_cell_model/simulations/007_noise/submit_test_EI.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''Run a network for a short time; save data and plot activity.'''
-import numpy as np
+from __future__ import absolute_import, print_function, division
 
 from grid_cell_model.submitting.factory import SubmitterFactory
 from grid_cell_model.submitting.arguments import ArgumentCreator
@@ -9,6 +9,8 @@ from param_sweep import SubmissionParserBase
 from default_params import defaultParameters as dp
 
 parser = SubmissionParserBase()
+parser.add_argument('--nthreads', type=int, default=1,
+                    help='Number of simulation threads.')
 o = parser.parse_args()
 
 for noise_sigma in parser.noise_sigmas:
@@ -28,16 +30,14 @@ for noise_sigma in parser.noise_sigmas:
     dry_run     = o.dry_run
 
     p['master_seed'] = 123456
-    p['time']        = 600e3 if o.time is None else o.time  # ms
-    p['nthreads']    = 1
+    p['time']        = 10e3 if o.time is None else o.time  # ms
+    p['nthreads']    = o.nthreads
+    p['ntrials']     = o.ntrials
     p['verbosity']   = o.verbosity
     p['Ivel']        = 50.  # mA
 
-    ac = ArgumentCreator(p, printout=True)
-    iterparams = {'trial': np.arange(o.ntrials)}
-    ac.insertDict(iterparams, mult=True)
-
     ###########################################################################
+    ac = ArgumentCreator(p, printout=True)
     submitter = SubmitterFactory.getSubmitter(
         ac, appName, envType=ENV, rtLimit=rtLimit, output_dir=simRootDir,
         label=simLabel, blocking=blocking, timePrefix=timePrefix,
@@ -45,4 +45,3 @@ for noise_sigma in parser.noise_sigmas:
     ac.setOption('output_dir', submitter.outputDir())
     startJobNum = 0
     submitter.submitAll(startJobNum, numRepeat, dry_run=dry_run)
-    submitter.saveIterParams(iterparams, dry_run=dry_run)
--- grid_cell_model/simulations/common/simulation_test_network.py
+++ grid_cell_model/simulations/common/simulation_test_network.py
@@ -1,4 +1,8 @@
-'''Main simulation run: Run network with constant speed.'''
+'''Main simulation run: Run a test simulation run of a bump with constant speed.
+
+This simulation will overwrite any old files that are present in the output
+directory.
+'''
 from __future__ import absolute_import, print_function, division
 
 from nest.hl_api import NESTError
@@ -9,33 +13,36 @@ from grid_cell_model.models.seeds import TrialSeedGenerator
 from grid_cell_model.data_storage import DataStorage
 
 parser = getOptParser()
-parser.add_argument("--trial", type=int, required=True,
-                    help='Trial number.')
-parser.add_argument("--ispikes", type=int, choices=[0, 1], default=0,
-                    help="Whether to save spikes from the I population")
 (o, args) = parser.parse_args()
 
 output_fname = "{0}/{1}job{2:05}_output.h5".format(o.output_dir,
                                                    o.fileNamePrefix, o.job_num)
 d = DataStorage.open(output_fname, 'w')
+d['trials'] = []
 
 overalT = 0.
+stop = False
 ###############################################################################
-try:
-    seed_gen = TrialSeedGenerator(o.master_seed)
-    seed_gen.set_generators(o.trial)  # Each trial is reproducible
+seed_gen = TrialSeedGenerator(o.master_seed)
+for trial_idx in range(o.ntrials):
+    seed_gen.set_generators(trial_idx)  # Each trial is reproducible
     const_v = [0.0, -o.Ivel]
     ei_net = ConstantVelocityNetwork(o, simulationOpts=None, vel=const_v)
+    d['net_params'] = ei_net.getNetParams()  # Common settings will stay
 
-    ei_net.simulate(o.time, printTime=o.printTime)
+    try:
+        ei_net.simulate(o.time, printTime=o.printTime)
+    except NESTError as e:
+        print("Simulation interrupted. Message: {0}".format(str(e)))
+        print("Not saving the data. Trying to clean up if possible...")
+        stop = True
     ei_net.endSimulation()
-    d['Data'] = ei_net.getMinimalSaveData(ispikes=o.ispikes)
+    d['trials'].append(ei_net.getAllData())
     d.flush()
     constrT, simT, totalT = ei_net.printTimes()
     overalT += totalT
-except NESTError as e:
-    print("Simulation interrupted. Message: {0}".format(str(e)))
-    print("Not saving the data. Trying to clean up if possible...")
+    if stop:
+        break
 
 d.close()
 print("Script total run time: {0} s".format(overalT))
