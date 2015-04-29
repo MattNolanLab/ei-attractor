318150afac7639a4bff667abf87bd62a56042f55
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2015-04-29 10:24:49 +0100

    add (mainly) velocity figures to prob. connections plots

--- grid_cell_model/simulations/007_noise/figures/paper/probabilistic_connections/config.py
+++ grid_cell_model/simulations/007_noise/figures/paper/probabilistic_connections/config.py
@@ -7,16 +7,18 @@ import os.path
 import matplotlib.ticker as ti
 from noisefigs.plotters.base import SeparateMultipageSaver
 
+
+DATA_ROOT = ['simulation_data', 'submission', 'probabilistic_connections']
+
+
 def get_config():
     return _config
 
 
 _config = {
     'grids_data_root': None,
-    'bump_data_root': os.path.join('simulation_data', 'submission',
-                                    'probabilistic_connections',
-                                    'gamma_bump'),
-    'vel_data_root':  None,
+    'bump_data_root': os.path.join(*(DATA_ROOT + ['gamma_bump'])),
+    'vel_data_root':  os.path.join(*(DATA_ROOT + ['velocity'])),
     'const_pos_data_root': None,
     'singleDataRoot': None,
 
@@ -32,8 +34,15 @@ _config = {
     'GammaSweepsPlotter': {
         'plot_grid_contours': [0, 0, 0],
 
+        'AC_vmin': -0.15,
+        'AC_vmax': 0.6,
+
         'F_vmin': 30,
-        'F_vmax': 167,
+        'F_vmax': 142.9,
+
+        'F_cbar_kw': dict(
+            extend     = 'neither',
+        ),
     },
 
     'MaxPopulationFRSweepsPlotter': {
@@ -47,6 +56,20 @@ _config = {
 
     'BumpDriftAtTimePlotter': {
         'plot_grid_contours': [0, 0, 0],
+    },
+
+    'VelFitErrSweepPlotter': {
+        'plot_contours' : [0, 0, 0],
+    },
+
+    'VelFitStdSweepPlotter': {
+        'plot_contours' : [0, 0, 0],
+    },
+
+    'VelSlopeSweepPlotter': {
+        'plot_contours' : [0, 0, 0],
+        'vmin': -.24,
+        'vmax': 1.3306,
     },
 }
 
