974e7387c7fbed1658ddfb80215dc22e46af48cc
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2015-05-09 21:41:49 +0100

    add gamma/bump/seizure figures for E->E networks

---
+++ grid_cell_model/simulations/007_noise/figures/paper/ee_connections/config.py
@@ -0,0 +1,74 @@
+
+'''Configuration file for the noise paper.'''
+from __future__ import absolute_import, print_function
+
+import os.path
+
+import matplotlib.ticker as ti
+from noisefigs.plotters.base import SeparateMultipageSaver
+
+def get_config():
+    return _config
+
+
+ROOT_DIR = ['simulation_data', 'submission', 'ee_connections']
+
+_config = {
+    'grids_data_root': None,
+    'bump_data_root': os.path.join(*(ROOT_DIR + ['gamma_bump'])),
+    'vel_data_root':  None,
+    'const_pos_data_root': None,
+    'singleDataRoot': None,
+
+    'GridExampleRectPlotter': {
+        'fig_saver': SeparateMultipageSaver(None, 'pdf')
+    },
+
+
+    'MainBumpFormationPlotter': {
+        'plot_grid_contours': [0, 0, 0],
+    },
+
+    'GammaSweepsPlotter': {
+        'plot_grid_contours': [0, 0, 0],
+
+        'AC_vmin': -0.12,
+        'AC_vmax': 0.68,
+
+        'F_vmin': 30,
+        'F_vmax': 121.5,
+
+        'F_cbar_kw': dict(
+            extend = 'neither',
+        ),
+    },
+
+    'MaxPopulationFRSweepsPlotter': {
+        'plot_grid_contours': [0, 0, 0],
+    },
+
+    'PSeizureSweepPlotter': {
+        'plot_grid_contours': [0, 0, 0],
+        'FRThreshold': 300,
+    },
+
+    'BumpDriftAtTimePlotter': {
+        'plot_grid_contours': [0, 0, 0],
+    },
+
+    'VelFitErrSweepPlotter': {
+        'plot_contours': [0, 0, 0],
+        'vmin': 0.1,
+        'vmax': 12.101,
+    },
+
+    'VelFitStdSweepPlotter': {
+        'plot_contours': [0, 0, 0],
+    },
+
+    'VelSlopeSweepPlotter': {
+        'plot_contours': [0, 0, 0],
+        'vmin': -.6,
+        'vmax': 1.64,
+    },
+}
