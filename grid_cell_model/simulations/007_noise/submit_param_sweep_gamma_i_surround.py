048ab3114cffa408f840f32a7e114639df0f0f8c
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2015-05-16 11:07:02 +0100

    add g_uni_GABA_frac parameter to I-surround gamma sweeps script

--- grid_cell_model/simulations/007_noise/submit_param_sweep_gamma_i_surround.py
+++ grid_cell_model/simulations/007_noise/submit_param_sweep_gamma_i_surround.py
@@ -12,6 +12,9 @@ sweep = ParameterSweep('../common/simulation_stationary.py', dp)
 parser = sweep.parser
 parser.add_argument('--Iext_e_theta', type=float, help='Cosine theta input to E cells(pA).')
 parser.add_argument('--Iext_i_theta', type=float, help='Cosine theta input to I cells (pA).')
+parser.add_argument('--g_uni_GABA_frac', type=float,
+                    help='Strength or uniform I->E synapses relative to the '
+                         'structured I->E.')
 parser.add_argument('--g_AMPA_total', type=float)
 parser.add_argument('--g_GABA_total', type=float)
 parser.parse_args()
@@ -22,6 +25,8 @@ if parser.options.Iext_e_theta is not None:
     p['Iext_e_theta'] = parser.options.Iext_e_theta
 if parser.options.Iext_i_theta is not None:
     p['Iext_i_theta'] = parser.options.Iext_i_theta
+if parser.options.g_uni_GABA_frac is not None:
+    p['g_uni_GABA_frac'] = parser.options.g_uni_GABA_frac
 if parser.options.g_AMPA_total is not None:
     p['g_AMPA_total'] = parser.options.g_AMPA_total
 if parser.options.g_GABA_total is not None:
