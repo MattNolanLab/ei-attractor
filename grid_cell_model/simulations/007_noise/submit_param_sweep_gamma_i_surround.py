3375724e385f7bb5628918b925603eb1c2e8ab38
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2015-05-16 15:44:59 +0100

    add Iext_e/i_const params to I-surround gamma script

--- grid_cell_model/simulations/007_noise/submit_param_sweep_gamma_i_surround.py
+++ grid_cell_model/simulations/007_noise/submit_param_sweep_gamma_i_surround.py
@@ -11,7 +11,9 @@ from default_params import defaultParameters as dp
 sweep = ParameterSweep('../common/simulation_stationary.py', dp)
 parser = sweep.parser
 parser.add_argument('--Iext_e_theta', type=float, help='Cosine theta input to E cells(pA).')
+parser.add_argument('--Iext_e_const', type=float, help='Constant input to E cells(pA).')
 parser.add_argument('--Iext_i_theta', type=float, help='Cosine theta input to I cells (pA).')
+parser.add_argument('--Iext_i_const', type=float, help='Constant input to I cells(pA).')
 parser.add_argument('--g_uni_GABA_frac', type=float,
                     help='Strength or uniform I->E synapses relative to the '
                          'structured I->E.')
@@ -23,8 +25,12 @@ p = {}
 p['master_seed'] = 123456
 if parser.options.Iext_e_theta is not None:
     p['Iext_e_theta'] = parser.options.Iext_e_theta
+if parser.options.Iext_e_const is not None:
+    p['Iext_e_const'] = parser.options.Iext_e_const
 if parser.options.Iext_i_theta is not None:
     p['Iext_i_theta'] = parser.options.Iext_i_theta
+if parser.options.Iext_i_const is not None:
+    p['Iext_i_const'] = parser.options.Iext_i_const
 if parser.options.g_uni_GABA_frac is not None:
     p['g_uni_GABA_frac'] = parser.options.g_uni_GABA_frac
 if parser.options.g_AMPA_total is not None:
