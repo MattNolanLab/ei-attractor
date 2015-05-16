e31dfc1178cea5190fe64554ea548ead603d2877
Lukas Solanka <l.solanka@sms.ed.ac.uk>
2015-05-16 22:15:57 +0100

    add Iext parameters to I-suround velocity script

--- grid_cell_model/simulations/007_noise/submit_param_sweep_velocity_i_surround.py
+++ grid_cell_model/simulations/007_noise/submit_param_sweep_velocity_i_surround.py
@@ -23,9 +23,34 @@ from grid_cell_model.submitting.noise.templates import ParameterSweep
 from default_params import defaultParameters as dp
 
 sweep = ParameterSweep('simulation_velocity.py', dp)
+parser = sweep.parser
+parser.add_argument('--Iext_e_theta', type=float, help='Cosine theta input to E cells(pA).')
+parser.add_argument('--Iext_e_const', type=float, help='Constant input to E cells(pA).')
+parser.add_argument('--Iext_i_theta', type=float, help='Cosine theta input to I cells (pA).')
+parser.add_argument('--Iext_i_const', type=float, help='Constant input to I cells(pA).')
+parser.add_argument('--g_uni_GABA_frac', type=float,
+                    help='Strength or uniform I->E synapses relative to the '
+                         'structured I->E.')
+parser.add_argument('--g_AMPA_total', type=float)
+parser.add_argument('--g_GABA_total', type=float)
+parser.parse_args()
 
 p = {}
 p['master_seed'] = 123456
+if parser.options.Iext_e_theta is not None:
+    p['Iext_e_theta'] = parser.options.Iext_e_theta
+if parser.options.Iext_e_const is not None:
+    p['Iext_e_const'] = parser.options.Iext_e_const
+if parser.options.Iext_i_theta is not None:
+    p['Iext_i_theta'] = parser.options.Iext_i_theta
+if parser.options.Iext_i_const is not None:
+    p['Iext_i_const'] = parser.options.Iext_i_const
+if parser.options.g_uni_GABA_frac is not None:
+    p['g_uni_GABA_frac'] = parser.options.g_uni_GABA_frac
+if parser.options.g_AMPA_total is not None:
+    p['g_AMPA_total'] = parser.options.g_AMPA_total
+if parser.options.g_GABA_total is not None:
+    p['g_GABA_total'] = parser.options.g_GABA_total
 
 p['AMPA_gaussian'] = 1
 
